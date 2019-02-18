452281948227ab67e6594b2fbccb391824cc09fb
Connor McArthur <connor@fishtownanalytics.com>
2019-02-18 16:02:37 -0500

    Bump version: 0.13.0a1 → 0.13.0a2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from distutils.core import setup
 
 package_name = "dbt-bigquery"
-package_version = "0.13.0a1"
+package_version = "0.13.0a2"
 description = """The bigquery adapter plugin for dbt (data build tool)"""
 
 

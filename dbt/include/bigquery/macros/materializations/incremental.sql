23a298c912bd7a34a9e3d2969acd2f67e0b6bac0
Jacob Beck <jake@fishtownanalytics.com>
2019-04-26 09:36:45 -0600

    remove non-destructive mode

--- dbt/include/bigquery/macros/materializations/incremental.sql
+++ dbt/include/bigquery/macros/materializations/incremental.sql
@@ -3,12 +3,7 @@
 
   {%- set unique_key = config.get('unique_key') -%}
 
-  {%- set non_destructive_mode = (flags.NON_DESTRUCTIVE == True) -%}
   {%- set full_refresh_mode = (flags.FULL_REFRESH == True) -%}
-
-  {% if non_destructive_mode %}
-    {{ exceptions.raise_compiler_error("--non-destructive mode is not supported on BigQuery") }}
-  {% endif %}
 
   {%- set identifier = model['alias'] -%}
 

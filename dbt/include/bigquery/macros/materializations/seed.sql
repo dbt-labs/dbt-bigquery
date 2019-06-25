ff3ef2be61d688d516d63ed4957e5c418a212e7e
Jacob Beck <jake@fishtownanalytics.com>
2019-06-25 12:48:29 -0400

    set name back to agate_table in the sql

--- dbt/include/bigquery/macros/materializations/seed.sql
+++ dbt/include/bigquery/macros/materializations/seed.sql
@@ -1,15 +1,16 @@
 
-{% macro bigquery__create_csv_table(model, csv_table) %}
+{% macro bigquery__create_csv_table(model, agate_table) %}
     -- no-op
 {% endmacro %}
 
-{% macro bigquery__reset_csv_table(model, full_refresh, old_relation, csv_table) %}
+{% macro bigquery__reset_csv_table(model, full_refresh, old_relation, agate_table) %}
     {{ adapter.drop_relation(old_relation) }}
 {% endmacro %}
 
-{% macro bigquery__load_csv_rows(model, csv_table) %}
+{% macro bigquery__load_csv_rows(model, agate_table) %}
 
   {%- set column_override = model['config'].get('column_types', {}) -%}
-  {{ adapter.load_dataframe(model['database'], model['schema'], model['alias'], csv_table, column_override) }}
+  {{ adapter.load_dataframe(model['database'], model['schema'], model['alias'],
+  							agate_table, column_override) }}
 
 {% endmacro %}

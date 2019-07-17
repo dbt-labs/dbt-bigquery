04bb3809be5166c63cab9b27e6edb776799889aa
Drew Banin <drew@fishtownanalytics.com>
2019-07-16 23:24:19 -0400

    possible fix for re-used check cols on BQ

--- dbt/adapters/bigquery/connections.py
+++ dbt/adapters/bigquery/connections.py
@@ -202,15 +202,28 @@ class BigQueryConnectionManager(BaseConnectionManager):
 
     def execute(self, sql, auto_begin=False, fetch=None):
         # auto_begin is ignored on bigquery, and only included for consistency
-        _, iterator = self.raw_execute(sql, fetch=fetch)
+        query_job, iterator = self.raw_execute(sql, fetch=fetch)
 
         if fetch:
             res = self.get_table_from_response(iterator)
         else:
             res = dbt.clients.agate_helper.empty_table()
 
-        # If we get here, the query succeeded
-        status = 'OK'
+        if query_job.statement_type == 'CREATE_VIEW':
+            status = 'CREATE VIEW'
+
+        elif query_job.statement_type == 'CREATE_TABLE_AS_SELECT':
+            conn = self.get_thread_connection()
+            client = conn.handle
+            table = client.get_table(query_job.destination)
+            status = 'CREATE TABLE ({})'.format(table.num_rows)
+
+        elif query_job.statement_type in ['INSERT', 'DELETE', 'MERGE']:
+            status = '{} ({})'.format(query_job.statement_type, query_job.num_dml_affected_rows)
+
+        else:
+            status = 'OK'
+
         return status, res
 
     def create_bigquery_table(self, database, schema, table_name, callback,

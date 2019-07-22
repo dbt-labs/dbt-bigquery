eb0a2054ffebc5758f061276db8ab796d20592e6
Drew Banin <drew@fishtownanalytics.com>
2019-07-22 11:14:03 -0400

    snapshot surrogate key whitespace control

--- dbt/include/bigquery/macros/materializations/snapshot.sql
+++ dbt/include/bigquery/macros/materializations/snapshot.sql
@@ -1,7 +1,7 @@
 {% macro bigquery__snapshot_hash_arguments(args) -%}
-  to_hex(md5(concat({% for arg in args %}
+  to_hex(md5(concat({%- for arg in args -%}
     coalesce(cast({{ arg }} as string), ''){% if not loop.last %}, '|',{% endif -%}
-  {% endfor %}
+  {%- endfor -%}
   )))
 {%- endmacro %}
 

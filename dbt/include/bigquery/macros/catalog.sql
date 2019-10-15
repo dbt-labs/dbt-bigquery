830d0121d598a50a7c16102767ce1d78417a3494
Drew Banin <drew@fishtownanalytics.com>
2019-10-14 23:07:42 -0400

    Include Location field in catalog from the SCHEMATA table

--- dbt/include/bigquery/macros/catalog.sql
+++ dbt/include/bigquery/macros/catalog.sql
@@ -4,7 +4,18 @@
   {%- call statement('catalog', fetch_result=True) -%}
     {% for information_schema in information_schemas %}
       (
-        with tables as (
+        with schemas as (
+
+            select
+              catalog_name as table_database,
+              schema_name as table_schema,
+              location
+
+            from `{{ information_schema.database }}`.INFORMATION_SCHEMA.SCHEMATA
+
+        ),
+
+        tables as (
             select
                 project_id as table_database,
                 dataset_id as table_schema,
@@ -102,7 +113,7 @@
         columns as (
 
             select * except (base_column_name)
-            from info_schema_columns 
+            from info_schema_columns
             join info_schema_column_paths using (relation_id, base_column_name)
 
         ),
@@ -151,6 +162,11 @@
             columns.column_type,
             columns.column_comment,
 
+            'Location' as `stats__location__label`,
+            location as `stats__location__value`,
+            'The geographic location of this table' as `stats__location__description`,
+            location is not null as `stats__location__include`,
+
             'Shard count' as `stats__date_shards__label`,
             table_shards.shard_count as `stats__date_shards__value`,
             'The number of date shards in this table' as `stats__date_shards__description`,
@@ -190,6 +206,7 @@
         -- sure that column metadata is picked up through the join. This will only
         -- return the column information for the "max" table in a date-sharded table set
         from unsharded_tables
+        left join schemas using(table_database, table_schema)
         left join columns using (relation_id)
         left join column_stats using (relation_id)
       )

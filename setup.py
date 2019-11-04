c6ccf7e216f05ef4ae94274d9a9386709eee0b77
Jacob Beck <jake@fishtownanalytics.com>
2019-11-04 09:24:31 -0700

    add some arbitrary-ish classifiers (importantly: the versions we support)

--- setup.py
+++ setup.py
@@ -33,4 +33,17 @@ setup(
         'google-cloud-bigquery>=1.15.0,<2',
     ],
     zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )

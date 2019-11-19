33b2905c211d0ed56817cff19a09b6ca330d1cd8
Connor McArthur <connor@fishtownanalytics.com>
2019-11-19 11:08:23 -0500

    Bump version: 0.15.0rc1 → 0.15.0rc2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 import os
 
 package_name = "dbt-bigquery"
-package_version = "0.15.0rc1"
+package_version = "0.15.0rc2"
 description = """The bigquery adapter plugin for dbt (data build tool)"""
 
 this_directory = os.path.abspath(os.path.dirname(__file__))

af0e75ba3dab9c2e08c15b049920f8250f8eef96
Jacob Beck <jake@fishtownanalytics.com>
2020-03-04 14:20:05 -0700

    Bump version: 0.16.0b3 → 0.17.0a1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 
 package_name = "dbt-bigquery"
-package_version = "0.16.0rc2"
+package_version = "0.17.0a1"
 description = """The bigquery adapter plugin for dbt (data build tool)"""
 
 this_directory = os.path.abspath(os.path.dirname(__file__))

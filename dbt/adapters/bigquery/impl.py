f5087ddfedf3dd3f52fa59ec30b16a3986b492d4
Azhar Dewji <azhardewji@gmail.com>
2020-07-29 17:19:54 -0400

    Addressed PR comments

--- dbt/adapters/bigquery/impl.py
+++ dbt/adapters/bigquery/impl.py
@@ -612,10 +612,10 @@ class BigQueryAdapter(BaseAdapter):
         if dotted_column_name in dbt_columns:
             column_config = dbt_columns[dotted_column_name]
             bq_column_dict['description'] = column_config.get('description')
-            if column_config.get('tags'):
-                bq_column_dict['policyTags'] = {}
-                bq_column_dict['policyTags']['names'] = \
-                    column_config.get('tags')
+            if column_config.get('policy_tags'):
+                bq_column_dict['policyTags'] = {
+                    'names': column_config.get('policy_tags')
+                }
 
         new_fields = []
         for child_col_dict in bq_column_dict.get('fields', list()):

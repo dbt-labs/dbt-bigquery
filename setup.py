0ae7cc6b2b6c75d0166144b791d81fc2a1f14fc7
Maximilian Roos <m@maxroos.com>
2020-11-11 13:48:41 -0800

    _

--- setup.py
+++ setup.py
@@ -48,10 +48,12 @@ setup(
     install_requires=[
         'dbt-core=={}'.format(package_version),
         'protobuf>=3.13.0,<4',
-        'google-cloud-core>=1.3.0,<2',
-        'google-cloud-bigquery>=1.25.0,<3',
-        'google-api-core>=1.16.0,<2',
-        'googleapis-common-protos>=1.6.0,<2',
+        # These are more tightly pinned, as they have a track record of
+        # breaking changes in minor releases.
+        'google-cloud-core>=1.3.0,<1.5',
+        'google-cloud-bigquery>=1.25.0,<2.4',
+        'google-api-core>=1.16.0,<1.24',
+        'googleapis-common-protos>=1.6.0,<1.53',
         'six>=1.14.0',
     ],
     zip_safe=False,

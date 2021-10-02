240230b4de542fe3549f69d469f13b418dd335a6
Jeremy Cohen <jeremy@dbtlabs.com>
2021-10-02 16:37:43 +0200

    setup.py, tox, GHA

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 import os
 import sys
+import re
 
+# require python 3.6 or newer
 if sys.version_info < (3, 6):
     print('Error: dbt does not support this version of Python.')
     print('Please upgrade to Python 3.6 or higher.')
     sys.exit(1)
 
 
+# require version of setuptools that supports find_namespace_packages
 from setuptools import setup
 try:
     from setuptools import find_namespace_packages
@@ -19,13 +22,44 @@ except ImportError:
     sys.exit(1)
 
 
-package_name = "dbt-bigquery"
-package_version = "0.21.0rc1"
-description = """The bigquery adapter plugin for dbt (data build tool)"""
-
+# pull long description from README
 this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
+
+
+# get this package's version from dbt/adapters/<name>/__version__.py
+def _get_plugin_version_dict():
+    _version_path = os.path.join(
+        this_directory, 'dbt', 'adapters', 'bigquery', '__version__.py'
+    )
+    _semver = r'''(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)'''
+    _pre = r'''((?P<prekind>a|b|rc)(?P<pre>\d+))?'''
+    _version_pattern = fr'''version\s*=\s*["']{_semver}{_pre}["']'''
+    with open(_version_path) as f:
+        match = re.search(_version_pattern, f.read().strip())
+        if match is None:
+            raise ValueError(f'invalid version at {_version_path}')
+        return match.groupdict()
+
+
+def _get_plugin_version():
+    parts = _get_plugin_version_dict()
+    return "{major}.{minor}.{patch}{prekind}{pre}".format(**parts)
+
+
+# require a compatible minor version (~=), prerelease if this is a prerelease
+def _get_dbt_core_version():
+    parts = _get_plugin_version_dict()
+    minor = "{major}.{minor}.0".format(**parts)
+    pre = (parts["prekind"]+"1" if parts["prekind"] else "")
+    return f"{minor}{pre}"
+
+
+package_name = "dbt-bigquery"
+package_version = _get_plugin_version()
+dbt_core_version = _get_dbt_core_version()
+description = """The BigQuery adapter plugin for dbt"""
 
 setup(
     name=package_name,
@@ -37,16 +71,9 @@ setup(
     author_email="info@dbtlabs.com",
     url="https://github.com/dbt-labs/dbt-bigquery",
     packages=find_namespace_packages(include=['dbt', 'dbt.*']),
-    package_data={
-        'dbt': [
-            'include/bigquery/dbt_project.yml',
-            'include/bigquery/sample_profiles.yml',
-            'include/bigquery/macros/*.sql',
-            'include/bigquery/macros/**/*.sql',
-        ]
-    },
+    include_package_data=True,
     install_requires=[
-        'dbt-core=={}'.format(package_version),
+        'dbt-core~={}'.format(dbt_core_version),
         'protobuf>=3.13.0,<4',
         'google-cloud-core>=1.3.0,<2',
         'google-cloud-bigquery>=1.25.0,<3',

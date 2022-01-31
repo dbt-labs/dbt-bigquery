e91aacf60cee977cbecd1006dd2363fe9e8b7ae9
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-01-31 15:05:58 -0600

    sending test up to be checked by kyle

---
+++ tests/integration/bigquery_test/test_no_access.py
@@ -0,0 +1,19 @@
+from tests.integration.base import DBTIntegrationTest, use_profile
+
+class TestNoAccess(DBTIntegrationTest):
+
+    @property
+    def schema(self):
+        return 'no_access_models'
+
+    @property
+    def models(self):
+        return "no-access-models"
+    
+
+    @use_profile('bigquery')
+    def test_bigquery_no_access(self):
+        results = self.run_dbt(['run','--exclude','model_1'])
+        self.assertEqual(len(results), 1)
+        self.run_dbt(expect_pass=True)
+

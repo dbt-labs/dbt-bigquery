6ab5cec515bbbc2c09f974bb90a609d99116c4fc
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-02-22 14:47:00 -0600

    adding docstring and small comment on why we use the exception we do

--- tests/integration/bigquery_test/test_no_access.py
+++ tests/integration/bigquery_test/test_no_access.py
@@ -14,6 +14,7 @@ class TestNoAccess(DBTIntegrationTest):
     
     @use_profile('bigquery')
     def test_bigquery_no_access(self):
+        """tests error exceptions against project user doesn't have access to."""
         results = self.run_dbt(['run','--exclude','model_1'])
         self.assertEqual(len(results), 1)
         results = self.run_dbt(['run','--select','model_2'])
@@ -21,6 +22,7 @@ class TestNoAccess(DBTIntegrationTest):
         try:
             results = self.run_dbt(['run','--select','model_1'])
         except dbt.exceptions.DatabaseException:
+            # have to test against DatabaseException as run_dbt catches warehouse thorwn error and genreralizes it to dbt version.
             is_false = True
             self.assertTrue(is_false)
             self.assertRaises(dbt.exceptions.DatabaseException)

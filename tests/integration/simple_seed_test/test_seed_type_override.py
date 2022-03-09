791dcce26c14e9d02482b48cbb3570378e2b7acb
Darryl Ng <darryl.nmj@gmail.com>
2022-03-10 10:45:30 +1100

    Revert "[CT-264] [CT-262] [Feature] Support BigQuery labels for seeded models"
    This reverts commit b5d0f2feaeb12bd5c5aeceb12b50015afdeb0f9c.

--- tests/integration/simple_seed_test/test_seed_type_override.py
+++ tests/integration/simple_seed_test/test_seed_type_override.py
@@ -1,15 +1,11 @@
 from tests.integration.base import DBTIntegrationTest, use_profile
 
 
-class TestSimpleSeed(DBTIntegrationTest):
+class TestSimpleSeedColumnOverride(DBTIntegrationTest):
 
     @property
     def schema(self):
         return "simple_seed"
-
-    @property
-    def models(self):
-        return "models-bq"
 
     @property
     def project_config(self):
@@ -23,18 +19,21 @@ class TestSimpleSeed(DBTIntegrationTest):
                     'quote_columns': True,
                     'seed_enabled': {
                         'enabled': True,
-                        '+column_types': self.seed_enabled_types(),
+                        '+column_types': self.seed_enabled_types()
                     },
                     'seed_tricky': {
                         'enabled': True,
                         '+column_types': self.seed_tricky_types(),
                     },
-                    'seed_labels': {
-                        'enabled': True,
-                    },
                 },
             },
         }
+
+
+class TestSimpleSeedColumnOverrideBQ(TestSimpleSeedColumnOverride):
+    @property
+    def models(self):
+        return "models-bq"
 
     def seed_enabled_types(self):
         return {
@@ -53,36 +52,9 @@ class TestSimpleSeed(DBTIntegrationTest):
     def profile_config(self):
         return self.bigquery_profile()
 
-
-class TestSimpleSeedColumnOverrideBQ(TestSimpleSeed):
-
     @use_profile('bigquery')
     def test_bigquery_simple_seed_with_column_override_bigquery(self):
         results = self.run_dbt(["seed", "--show"])
-        self.assertEqual(len(results),  3)
+        self.assertEqual(len(results),  2)
         results = self.run_dbt(["test"])
         self.assertEqual(len(results),  10)
-
-
-class TestSimpleSeedConfigLabelsBQ(TestSimpleSeed):
-
-    @property
-    def table_labels(self):
-        return {
-            'contains_pii': 'yes',
-            'contains_pie': 'no'
-        }
-
-    @use_profile('bigquery')
-    def test__bigquery_seed_table_with_labels(self):
-        _ = self.run_dbt(["seed", "--show"])
-        with self.get_connection() as conn:
-            client = conn.handle
-
-            table = client.get_table(
-                self.adapter.connections.get_bq_table(
-                    self.default_database, self.unique_schema(), 'seed_labels')
-            )
-
-            self.assertTrue(table.labels)
-            self.assertEquals(table.labels, self.table_labels)

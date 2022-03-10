a426d3aaee7cb2b642fc0cbfe421d785c67529e3
pgoslatara <pgoslatara@gmail.com>
2022-03-10 22:03:37 +0100

    Adding upload_file adapter (#121)
    * Adding upload_file adapter
    
    * flake8 formatting
    
    * Replacing get_timeout with newer get_job_execution_timeout_seconds
    
    * Adding integration tests for upload_file macro
    
    * Removing conn arg from table_ref method
    
    * Updating schema method to upload_file
    
    * Adding checks on created tables
    
    * Correcting class name
    
    * Updating CHANGELOG.md
    
    Co-authored-by: Matthew McKnight <91097623+McKnight-42@users.noreply.github.com>

--- dbt/include/bigquery/macros/adapters.sql
+++ dbt/include/bigquery/macros/adapters.sql
@@ -214,3 +214,11 @@ group by unique_field
 having count(*) > 1
 
 {% endmacro %}
+
+{% macro bigquery__upload_file(local_file_path, database, table_schema, table_name) %}
+
+  {{ log("kwargs: " ~ kwargs) }}
+
+  {% do adapter.upload_file(local_file_path, database, table_schema, table_name, kwargs=kwargs) %}
+
+{% endmacro %}

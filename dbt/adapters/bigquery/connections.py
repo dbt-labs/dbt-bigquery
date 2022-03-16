30b33a45851464b9c5c0b033a4b348ad0c7ffc37
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-03-16 15:06:29 -0500

    clearing mypy errors

--- dbt/adapters/bigquery/connections.py
+++ dbt/adapters/bigquery/connections.py
@@ -474,7 +474,7 @@ class BigQueryConnectionManager(BaseConnectionManager):
             processed_bytes = self.format_bytes(bytes_processed)
             message = f"{code} ({num_rows_formated} rows, {processed_bytes} processed)"
 
-        response = BigQueryAdapterResponse(
+        response = BigQueryAdapterResponse(  # type: ignore[call-arg]
             _message=message, rows_affected=num_rows, code=code, bytes_processed=bytes_processed
         )
 

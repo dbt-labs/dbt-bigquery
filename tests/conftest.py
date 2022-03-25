8de9025070348d55fecc39a920c3420e55720b46
Gerda Shank <gerda@dbtlabs.com>
2022-03-25 10:27:00 -0400

    Remove unique_schema from dbt_profile_target

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ pytest_plugins = ["dbt.tests.fixtures.project"]
 
 # The profile dictionary, used to write out profiles.yml
 @pytest.fixture(scope="class")
-def dbt_profile_target(unique_schema):
+def dbt_profile_target():
     credentials_json_str = os.getenv('BIGQUERY_TEST_SERVICE_ACCOUNT_JSON').replace("'", '')
     credentials = json.loads(credentials_json_str)
     project_id = credentials.get('project_id')
@@ -19,5 +19,4 @@ def dbt_profile_target(unique_schema):
         'threads': 1,
         'project': project_id,
         'keyfile_json': credentials,
-        'schema': unique_schema,
     }

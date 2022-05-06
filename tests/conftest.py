0823e3b12e816b86b037e0dac7ecc705f43f9f3d
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-05-06 15:52:34 -0500

    removing conftest change

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,7 +29,6 @@ def oauth_target():
         'type': 'bigquery',
         'method': 'oauth',
         'threads': 1,
-        "project": os.getenv('BIGQUERY_TEST_DATABASE')
         # project isn't needed if you configure a default, via 'gcloud config set project'
     }
 

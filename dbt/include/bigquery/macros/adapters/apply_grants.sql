42ae831ace34a003132a128d6bd36dc4064fa0d9
Emily Rockman <emily.rockman@dbtlabs.com>
2022-07-07 10:07:52 -0500

    update macros, temp update to dev-req for CI

--- dbt/include/bigquery/macros/adapters/apply_grants.sql
+++ dbt/include/bigquery/macros/adapters/apply_grants.sql
@@ -1,27 +1,62 @@
 {% macro  bigquery__get_show_grant_sql(relation) %}
-{# Note: This only works if the location is defined in the profile.  It is an optional field right now. #}
-{# TODO: location is hardcoded for now - need logic around this (default to us)#}
-select * from {{ relation.project }}.`region-us`.INFORMATION_SCHEMA.OBJECT_PRIVILEGES
-where object_schema = "{{ relation.dataset }}" and object_name = "{{ relation.identifier }}"
+    select * from {{ relation.project }}.`region-{{ target.location }}`.INFORMATION_SCHEMA.OBJECT_PRIVILEGES
+    where object_schema = "{{ relation.dataset }}" and object_name = "{{ relation.identifier }}"
 {% endmacro %}
 
-{% macro bigquery__get_grant_sql(relation, grant_config) %}
-    {% for privilege in grant_config.keys() %}
-     {{ log('privilege: ' ~ privilege) }}
-        {% set grantees = grant_config[privilege] %}
-        {% for grantee in grantees %}
-            grant `{{ privilege }}` on {{ relation.type }} {{ relation.dataset }}.{{ relation.identifier }} to "{{ grantee }}";
-        {% endfor %}
-    {% endfor %}
-{% endmacro %}
+
+{%- macro bigquery__get_grant_sql(relation, grant_config) -%}
+    {%- for privilege in grant_config.keys() -%}
+        {%- set grantees = grant_config[privilege] -%}
+        {%- if grantees -%}
+            {%- for grantee in grantees -%}
+                grant {{ privilege }} on {{ relation.type }} {{ relation }} to {{ grantee}};
+            {%- endfor -%}
+        {%- endif -%}
+    {%- endfor -%}
+{%- endmacro %}
+
 
 {% macro bigquery__get_revoke_sql(relation, grant_config) %}
-{# TODO: will probably need to do some more tweaks around revoke and table fields once that is resolved in core #}
-    {% for privilege in grant_config.keys() %}
-        {% set grantees = grant_config[privilege] %}
-        {% for grantee in grantees if grantee !=  target.user %}
-            revoke `{{ privilege }}` on {{ relation.type }} {{ relation.dataset }}.{{ relation.identifier }} from "{{ grantee }}";
-        {% endfor %}
-    {% endfor %}
-{% endmacro %}
+    {%- for privilege in grant_config.keys() -%}
+        {%- set grantees = [] -%}
+        {%- set all_grantees = grant_config[privilege] -%}
+        {%- for grantee in all_grantees -%}
+            {%- if grantee != target.user -%}
+                {% do grantees.append(grantee) %}
+            {%- endif -%}
+        {% endfor -%}
+        {%- if grantees -%}
+            {%- for grantee in grantees -%}
+                revoke `{{ privilege }}` on {{ relation.type }} {{ relation.dataset }}.{{ relation.identifier }} from "{{ grantee }}";
+            {% endfor -%}
+        {%- endif -%}
+    {%- endfor -%}
+{%- endmacro -%}
 
+
+{% macro bigquery__apply_grants(relation, grant_config, should_revoke=True) %}
+    {% if grant_config %}
+            {% if should_revoke %}
+            {#-- This check is the only reason BQ needs its own copy of apply_grants --#}
+                {% if not target.location %}
+                    {{ exceptions.raise_compiler_error("In order to use the grants feature, you must specify a location ") }}
+                {% endif %}
+                {% set current_grants_table = run_query(get_show_grant_sql(relation)) %}
+                {% set current_grants_dict = adapter.standardize_grants_dict(current_grants_table) %}
+                {% set needs_granting = diff_of_two_dicts(grant_config, current_grants_dict) %}
+                {% set needs_revoking = diff_of_two_dicts(current_grants_dict, grant_config) %}
+                {% if not (needs_granting or needs_revoking) %}
+                    {{ log('All grants are in place, no revocation or granting needed.')}}
+                {% endif %}
+            {% else %}
+                {% set needs_revoking = {} %}
+                {% set needs_granting = grant_config %}
+            {% endif %}
+            {% if needs_granting or needs_revoking %}
+                {% call statement('grants') %}
+                    {{ get_revoke_sql(relation, needs_revoking) }}
+                    {{ get_grant_sql(relation, needs_granting) }}
+                {% endcall %}
+            {% endif %}
+    {% endif %}
+{% endmacro %}

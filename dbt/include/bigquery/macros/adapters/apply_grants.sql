d8f6e2d6e3412a0c9a28bffa76fc10debb52fab3
Emily Rockman <emily.rockman@dbtlabs.com>
2022-07-08 14:34:35 -0500

    wip, broken tests

--- dbt/include/bigquery/macros/adapters/apply_grants.sql
+++ dbt/include/bigquery/macros/adapters/apply_grants.sql
@@ -9,20 +9,33 @@
 
 
 {%- macro bigquery__get_grant_sql(relation, grant_config) -%}
-    {%- for privilege in grant_config.keys() -%}
+    {%- set grant_statements = [] -%}
+    {%- for privilege in grant_config.keys() %}
         {%- set grantees = grant_config[privilege] -%}
-        {%- if grantees -%}
-            grant `{{ privilege }}` on {{ relation.type }} {{ relation }} to {{ '\"' + grantees|join('\", \"') + '\"' }};
+        {%- if grantees %}
+            {% set grant_sql -%}
+                grant `{{ privilege }}` on {{ relation.type }} {{ relation }} to {{ '\"' + grantees|join('\", \"') + '\"' }}
+            {%- endset %}
+            {%- do grant_statements.append(grant_sql) -%}
         {% endif -%}
     {%- endfor -%}
+    {{ return(grant_statements) }}
+
 {%- endmacro %}
 
 
 {% macro bigquery__get_revoke_sql(relation, grant_config) %}
+
+    {%- set revoke_statements = [] -%}
     {%- for privilege in grant_config.keys() -%}
         {%- set grantees = grant_config[privilege] -%}
-        {%- if grantees -%}
-            revoke `{{ privilege }}` on {{ relation.type }} {{ relation }} from {{ '\"' + grantees|join('\", \"') + '\"' }};
+        {%- if grantees %}
+            {% set revoke_sql -%}
+            revoke `{{ privilege }}` on {{ relation.type }} {{ relation }} from {{ '\"' + grantees|join('\", \"') + '\"' }}
+            {%- endset %}
+            {%- do revoke_statements.append(revoke_sql) -%}
         {% endif -%}
     {%- endfor -%}
+    {{ return(revoke_statements) }}
+
 {%- endmacro -%}

5ef5dd75a0e8d6e2b0fbfc0ca3710b8cd5f8dd1a
Doug Beatty <doug.beatty@dbtlabs.com>
2022-07-11 16:25:29 -0600

    Multiple options for rendering sql for grants and revokes

--- dbt/include/bigquery/macros/adapters/apply_grants.sql
+++ dbt/include/bigquery/macros/adapters/apply_grants.sql
@@ -11,10 +11,76 @@
 {% endmacro %}
 
 
-{%- macro bigquery__get_grant_sql(relation, privilege, grantee) -%}
+{%- macro bigquery__get_grant_sql(relation, privilege, grantees) -%}
+    {{ bigquery__get_grant_sql__option_3(relation, privilege, grantees) }}
+{%- endmacro -%}
+
+
+{%- macro bigquery__get_revoke_sql(relation, privilege, grantees) -%}
+    {{ bigquery__get_revoke_sql__option_3(relation, privilege, grantees) }}
+{%- endmacro -%}
+
+
+{%- macro bigquery__get_grant_sql__option_0(relation, privilege, grantee) -%}
     grant `{{ privilege }}` on {{ relation.type }} {{ relation }} to {{ '\"' + grantee|join('\", \"') + '\"' }}
 {%- endmacro -%}
 
-{%- macro bigquery__get_revoke_sql(relation, privilege, grantee) -%}
+
+{%- macro bigquery__get_revoke_sql__option_0(relation, privilege, grantee) -%}
     revoke `{{ privilege }}` on {{ relation.type }} {{ relation }} from {{ '\"' + grantee|join('\", \"') + '\"' }}
+{%- endmacro -%}
+
+
+{%- macro bigquery__get_grant_sql__option_1(relation, privilege, grantees) -%}
+    grant {{ adapter.quote(privilege) }} on {{ relation.type }} {{ relation }} to {{ string_literal_csv(grantees) }}
+{%- endmacro -%}
+
+
+{%- macro bigquery__get_revoke_sql__option_1(relation, privilege, grantees) -%}
+    revoke {{ adapter.quote(privilege) }} on {{ relation.type }} {{ relation }} from {{ string_literal_csv(grantees) }}
+{%- endmacro -%}
+
+
+{%- macro string_literal_csv(items) -%}
+    {%- for item in items -%}
+        {{ string_literal(item) }}{% if not loop.last %}, {% endif %}
+    {% endfor %}
+{%- endmacro -%}
+
+
+{%- macro bigquery__get_grant_sql__option_2(relation, privilege, grantees) -%}
+    grant {{ adapter.quote(privilege) }} on {{ relation.type }} {{ relation }} to {{ apply_string_literal(grantees) | join(', ') }}
+{%- endmacro -%}
+
+
+{%- macro bigquery__get_revoke_sql__option_2(relation, privilege, grantees) -%}
+    revoke {{ adapter.quote(privilege) }} on {{ relation.type }} {{ relation }} from {{ apply_string_literal(grantees) | join(', ') }}
+{%- endmacro -%}
+
+
+{%- macro apply_string_literal(items) -%}
+    {%- set apply_items = [] -%}
+    {%- for item in items -%}
+        {{ apply_items.append(string_literal(item)) }}
+    {% endfor %}
+    {%- do return(apply_items) -%}
+{%- endmacro -%}
+
+
+{%- macro bigquery__get_grant_sql__option_3(relation, privilege, grantees) -%}
+    grant {{ adapter.quote(privilege) }} on {{ relation.type }} {{ relation }} to {{ apply(string_literal, grantees) | join(', ') }}
+{%- endmacro -%}
+
+
+{%- macro bigquery__get_revoke_sql__option_3(relation, privilege, grantees) -%}
+    revoke {{ adapter.quote(privilege) }} on {{ relation.type }} {{ relation }} from {{ apply(string_literal, grantees) | join(', ') }}
+{%- endmacro -%}
+
+
+{%- macro apply(macro, items) -%}
+    {%- set apply_items = [] -%}
+    {%- for item in items -%}
+        {{ apply_items.append(macro(item)) }}
+    {% endfor %}
+    {%- do return(apply_items) -%}
 {%- endmacro -%}

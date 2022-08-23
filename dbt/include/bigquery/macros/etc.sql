cfb75f75f24ac18772078e41d2b9780fd94b4d43
Judah Rand <17158624+judahrand@users.noreply.github.com>
2022-08-23 18:48:35 +0100

    Add macro which returns the largest partition

--- dbt/include/bigquery/macros/etc.sql
+++ dbt/include/bigquery/macros/etc.sql
@@ -13,3 +13,34 @@
   {%- endif -%}
   {{- return(None) -}}
 {%- endmacro -%}
+
+{%- macro get_max_partition(relation) -%}
+    {%- if not execute -%}
+        {{- return(none) -}}
+    {%- endif -%}
+
+    {# Get info about partitioning. #}
+    {%- set raw_partition_by = config.get('partition_by', none) -%}
+    {%- set partition_config = adapter.parse_partition_by(raw_partition_by) -%}
+
+    {# Get the maxiumum partition id. #}
+    {%- set partitions_metadata = get_partitions_metadata(relation) -%}
+    {%- set partition_id = partitions_metadata.columns['partition_id'].values() | max -%}
+
+    {# Format partition id for SQL comparision. #}
+    {%- if partition_config.data_type | lower in ('date', 'timestamp', 'datetime') -%}
+        {%- if partition_id | length == 4 -%}
+            {%- set format = '%Y' -%}
+        {%- elif partition_id | length == 6 -%}
+            {%- set format = '%Y%m' -%}
+        {%- elif partition_id | length == 8 -%}
+            {%- set format = '%Y%m%d' -%}
+        {%- else -%}
+            {%- set format = '%Y%m%d%H' -%}
+        {%- endif -%}
+        {%- set res = dbt.safe_cast("parse_timestamp('{}', '{}')".format(format, partition_id), partition_config.data_type) -%}
+    {%- else -%}
+        {%- set res = dbt.safe_cast("'{}'".format(partition_id), dbt.type_int()) -%}
+    {%- endif -%}
+    {{- return(res) -}}
+{%- endmacro -%}

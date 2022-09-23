d7e3e42fd9ac6a5cac44fb5ffbb0d73e67136b5f
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-09-23 15:05:18 -0500

    readding ref to MACROS__EXPECT_VALUE_SQL as seems to be overwritten when adding new bigquery macro

--- tests/functional/adapter/test_aliases.py
+++ tests/functional/adapter/test_aliases.py
@@ -1,7 +1,7 @@
 import pytest
 from dbt.tests.adapter.aliases.test_aliases import BaseAliases, BaseSameAliasDifferentSchemas
 
-_MACROS__BIGQUERY_CAST_SQL = """
+MACROS__BIGQUERY_CAST_SQL = """
 
 {% macro bigquery__string_literal(s) %}
     cast('{{ s }}' as string)
@@ -9,16 +9,27 @@ _MACROS__BIGQUERY_CAST_SQL = """
 
 """
 
+MACROS__EXPECT_VALUE_SQL = """
+
+-- cross-db compatible test, similar to accepted_values
+
+{% test expect_value(model, field, value) %}
+
+select *
+from {{ model }}
+where {{ field }} != '{{ value }}'
+
+{% endtest %}
+
+"""
+
 class TestAliasesBigQuery(BaseAliases):
     @pytest.fixture(scope="class")
     def macros(self):
-        return {"bigquery_cast.sql": _MACROS__BIGQUERY_CAST_SQL}
+        return {"bigquery_cast.sql": MACROS__BIGQUERY_CAST_SQL, "expect_value.sql": MACROS__EXPECT_VALUE_SQL}
 
-    pass
 
 class TestSameTestSameAliasDifferentSchemasBigQuery(BaseSameAliasDifferentSchemas):
     @pytest.fixture(scope="class")
     def macros(self):
-        return {"bigquery_cast.sql": _MACROS__BIGQUERY_CAST_SQL}
-
-    pass
+        return {"bigquery_cast.sql": MACROS__BIGQUERY_CAST_SQL, "expect_value.sql": MACROS__EXPECT_VALUE_SQL}

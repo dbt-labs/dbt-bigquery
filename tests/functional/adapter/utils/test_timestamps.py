bbe14ae43aa770932b9cef6db1e5ef50af4bc0c8
Colin <colin.rogers@dbtlabs.com>
2022-09-26 14:40:59 -0700

    update expected test fixture

--- tests/functional/adapter/utils/test_timestamps.py
+++ tests/functional/adapter/utils/test_timestamps.py
@@ -5,7 +5,4 @@ from dbt.tests.adapter.utils import test_timestamps
 class TestCurrentTimestampSnowflake(test_timestamps.TestCurrentTimestamps):
     @pytest.fixture(scope="class")
     def expected_schema(self):
-        return {
-                "CURRENT_TIMESTAMP": "TIMESTAMP",
-                "CURRENT_TIMESTAMP_IN_UTC": "TIMESTAMP"
-            }
+        return {"current_timestamp": "TIMESTAMP", "current_timestamp_in_utc": "TIMESTAMP"}

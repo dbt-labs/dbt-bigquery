1ef5c5fc92d4bebf6dac03909c6f6d2de7afc8ae
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-09-27 10:17:47 -0500

    minor import change, rechanging back to uique_schema version of test pulled in from core

--- tests/functional/adapter/test_aliases.py
+++ tests/functional/adapter/test_aliases.py
@@ -1,6 +1,6 @@
 import pytest
 import os
-from dbt.tests.adapter.aliases.test_aliases import BaseAliases, BaseSameAliasDifferentDatabases
+import dbt.tests.adapter.aliases.test_aliases as dbt_tests
 
 MACROS__BIGQUERY_CAST_SQL = """
 
@@ -24,25 +24,13 @@ where {{ field }} != '{{ value }}'
 
 """
 
-class TestAliasesBigQuery(BaseAliases):
+class TestAliasesBigQuery(dbt_tests.BaseAliases):
     @pytest.fixture(scope="class")
     def macros(self):
         return {"bigquery_cast.sql": MACROS__BIGQUERY_CAST_SQL, "expect_value.sql": MACROS__EXPECT_VALUE_SQL}
 
 
-class TestSameTestSameAliasDifferentDatabasesBigQuery(BaseSameAliasDifferentDatabases):
-    @pytest.fixture(scope="class")
-    def project_config_update(self):
-        return {
-            "config-version": 2,
-            "macro-paths": ["macros"],
-            "models": {
-                "test": {
-                    "alias": "duped_alias",
-                    "model_b": {"database": os.getenv("BIGQUERY_TEST_ALT_DATABASE")},
-                },
-            },
-        }
+class TestSameTestSameAliasDifferentDatabasesBigQuery(dbt_tests.BaseSameAliasDifferentDatabases):
     @pytest.fixture(scope="class")
     def macros(self):
         return {"bigquery_cast.sql": MACROS__BIGQUERY_CAST_SQL, "expect_value.sql": MACROS__EXPECT_VALUE_SQL}

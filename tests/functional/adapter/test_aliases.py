001676e7747776c42c80efe76bd893aa5308efcb
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-09-27 14:02:44 -0500

    rolling back test to unforked state will document thoughts on what problem is to raise as this may tie into other tests having hanging tables

--- tests/functional/adapter/test_aliases.py
+++ tests/functional/adapter/test_aliases.py
@@ -1,6 +1,6 @@
 import pytest
 import os
-import dbt.tests.adapter.aliases.test_aliases as dbt_tests
+from dbt.tests.adapter.aliases.test_aliases import BaseAliases, BaseSameAliasDifferentDatabases
 
 MACROS__BIGQUERY_CAST_SQL = """
 
@@ -24,13 +24,25 @@ where {{ field }} != '{{ value }}'
 
 """
 
-class TestAliasesBigQuery(dbt_tests.BaseAliases):
+class TestAliasesBigQuery(BaseAliases):
     @pytest.fixture(scope="class")
     def macros(self):
         return {"bigquery_cast.sql": MACROS__BIGQUERY_CAST_SQL, "expect_value.sql": MACROS__EXPECT_VALUE_SQL}
 
 
-class TestSameTestSameAliasDifferentDatabasesBigQuery(dbt_tests.BaseSameAliasDifferentDatabases):
+class TestSameTestSameAliasDifferentDatabasesBigQuery(BaseSameAliasDifferentDatabases):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "config-version": 2,
+            "macro-paths": ["macros"],
+            "models": {
+                "test": {
+                    "alias": "duped_alias",
+                    "model_b": {"database": os.getenv("BIGQUERY_TEST_ALT_DATABASE")},
+                },
+            },
+        }
     @pytest.fixture(scope="class")
     def macros(self):
         return {"bigquery_cast.sql": MACROS__BIGQUERY_CAST_SQL, "expect_value.sql": MACROS__EXPECT_VALUE_SQL}

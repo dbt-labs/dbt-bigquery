0432e338fe7df5229cffe6b393ae03d78d82c8a7
Colin <colin.rogers@dbtlabs.com>
2022-11-22 18:36:49 -0800

    Add unit tests

--- dbt/adapters/bigquery/dataset.py
+++ dbt/adapters/bigquery/dataset.py
@@ -1,3 +1,4 @@
+import threading
 import google.cloud.bigquery
 import queue
 
@@ -5,34 +6,42 @@ from threading import Thread
 from typing import Dict
 
 from dbt.events import AdapterLogger
-DATASET_UPDATE_QUEUE: queue.SimpleQueue = queue.SimpleQueue()
-__DATASET_UPDATE_THREAD: Dict[str, Thread] = {}
 
-logger = AdapterLogger("BigQuery")
+__DATASET_UPDATE_QUEUE = queue.SimpleQueue()
+__DATASET_UPDATE_THREAD: Dict[str, Thread] = {}
+__DATASET_UPDATE_LOCK = threading.Lock()
+
+logger: AdapterLogger = AdapterLogger("BigQuery")
+
 
 def update_dataset(client: google.cloud.bigquery.Client, update):
-        start_dataset_update_thread(client)
-        DATASET_UPDATE_QUEUE.put(update)
+    _start_dataset_update_thread(client)
+    __DATASET_UPDATE_QUEUE.put(update)
 
 
-def start_dataset_update_thread(client: google.cloud.bigquery.Client) -> None:
-    if "thread" in __DATASET_UPDATE_THREAD:
-        dataset_update_thread: Thread = __DATASET_UPDATE_THREAD["thread"]
-    dataset_update_thread = DatasetAsyncUpdater(client=client)
-    dataset_update_thread.start()
-    __DATASET_UPDATE_THREAD["thread"] = dataset_update_thread
+def _start_dataset_update_thread(client: google.cloud.bigquery.Client) -> None:
+    with __DATASET_UPDATE_LOCK:
+        if "thread" not in __DATASET_UPDATE_THREAD:
+            dataset_update_thread = DatasetAsyncUpdater(client=client, queue=__DATASET_UPDATE_QUEUE)
+            dataset_update_thread.start()
+            __DATASET_UPDATE_THREAD["thread"] = dataset_update_thread
 
 
 class DatasetAsyncUpdater(Thread):
     client: google.cloud.bigquery.Client
+    _queue: queue.SimpleQueue
 
-    def __init__(self, client) -> None:
+    def __init__(self, client, queue) -> None:
         super().__init__()
-        self.daemon = True # will close when the parent process closes
+        self.daemon = True  # will close when the parent process closes
         self.client = client
+        self._queue = queue
 
     def run(self) -> None:
         while True:
-            val = DATASET_UPDATE_QUEUE.get()
+            val = self._queue.get()
             if val:
-                self.client.update_dataset(**val)
+                try:
+                    self.client.update_dataset(*val)
+                except Exception as e:
+                    logger.error(e)
--- tests/unit/test_dataset.py
+++ tests/unit/test_dataset.py
@@ -1,11 +1,48 @@
-import unittest
+from time import sleep
+import pytest
 from unittest.mock import MagicMock
 
 from dbt.adapters.bigquery.dataset import update_dataset
+from dbt.adapters.bigquery import dataset
 
-class TestBigQueryDatasetUpdate(unittest.TestCase):
-    mock_client = MagicMock()
+@pytest.fixture
+def mock_client():
+    return MagicMock()
 
-    def test_start_dataset_update_thread(self):
-        update_dataset(self.mock_client)
-        self.mock_client.assert_called_once()
+@pytest.fixture
+def mock_dataset():
+    return MagicMock()
+
+
+def test_update_dataset_calls_bq_client_correctly(mock_client, mock_dataset):
+    update_request = (mock_dataset, ["access_entries"])
+    update_dataset(mock_client, update_request)
+    sleep(1)
+    mock_client.update_dataset.assert_called_once_with(*update_request)
+    update_dataset(mock_client, update_request)
+    sleep(1)
+    assert mock_client.update_dataset.call_count == 2
+
+
+def test_update_dataset_handles_bq_client_error(mock_client, mock_dataset, monkeypatch):
+    expected_error = ValueError("something went wrong")
+    mock_client.update_dataset.side_effect = expected_error
+    #we need to overwrite the updater's client with our new mock
+    dataset.__DATASET_UPDATE_THREAD["thread"].client = mock_client
+    update_request = (mock_dataset, ["access_entries"])
+    mock_logger = MagicMock()
+    monkeypatch.setattr(dataset, "logger", mock_logger)
+    update_dataset(mock_client, update_request)
+    update_dataset(mock_client, update_request)
+    sleep(1)
+    mock_logger.error.assert_called_with(expected_error)
+    assert mock_logger.error.call_count == 2
+    
+    
+def test_update_dataset_reuses_client(mock_client, mock_dataset):
+    mock_client_2 = MagicMock()
+    update_request = (mock_dataset, ["access_entries"])
+    update_dataset(mock_client, update_request)
+    update_dataset(mock_client_2, update_request)
+    sleep(1)
+    mock_client_2.update_dataset.assert_not_called()

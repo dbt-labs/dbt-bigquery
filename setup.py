6c989d093144d3f8f7d46a5040d6d2db4ebcfa4b
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-12-14 11:16:29 -0600

    playing with ranges for requirements

--- setup.py
+++ setup.py
@@ -70,12 +70,12 @@ setup(
     install_requires=[
         f"dbt-core~={_dbt_core_version(_dbt_bigquery_version())}",
         "protobuf>=3.13.0,<4",
-        "google-cloud-core~=2.3.0",
-        "google-cloud-bigquery~=1.25.0",
+        "google-cloud-core~=2.3",
+        "google-cloud-bigquery~=1.25",
         "google-api-core~=1.16.0",
-        "googleapis-common-protos~=1.6.0,<2",
-        "google-cloud-storage~=2.4.0",
-        "google-cloud-dataproc~=4.0.3",
+        "googleapis-common-protos>=1.6.0,<2",
+        "google-cloud-storage>=2.4.0",
+        "google-cloud-dataproc>=4.0.3",
     ],
     zip_safe=False,
     classifiers=[

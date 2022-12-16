537b56d10fcd897660ad21b5dcd35575c04bbaa4
Matthew McKnight <91097623+McKnight-42@users.noreply.github.com>
2022-12-16 11:22:40 -0600

    dbt-bigquery python 3.11 support  (#431)
    * initial pr for bigquery support for 3.11py
    
    * update pr number
    
    * add michelleark to core-team ignore list for changie
    
    * trying to pin some requirements to fix install
    
    * repin tox
    
    * loosening pins
    
    * loosening constraints in setup.py
    
    * playing with pins in setup.py
    
    * remove additions to team ignore
    
    * loosening google-cloud-cor
    
    * playing with setup.py ranges
    
    * loosening dataproc ranges
    
    * changing soft pin range for setup.py
    
    * reverting setup.py requred installs
    
    * remove duplicated protobuf
    
    * trying to loosen google-cloud-core
    
    * trying to loosen google-cloud-core
    
    * unloosening google-cloud-core
    
    * typo fix
    
    * google-cloud-core range setting to 2.3.0 and up
    
    * loosening google-cloud-bigquery
    
    * loosening dataproc require installs in setup.py
    
    * loosen all google required installs
    
    * playing with ranges for requirements
    
    * trying removing package versions to allow pip to attempt to solve dependency conflict
    
    * trying to tighten requirement versions some
    
    * loosen wheel install
    
    * loosening refs in requirements
    
    * removing caps in setup.py
    
    * minor change for wheel
    
    * trying to unbind installs
    
    * lossening setup.py to make sure dev-requirements works as is
    
    * typo
    
    * changing range for common-protos
    
    * changing range for common-protos
    
    * setting lower bound of versions in setup.py
    
    * double checking versioning
    
    * testing changes for google-cloud-core
    
    * testing changes for google-cloud-core
    
    * change to google-api-core versioning
    
    * changing versioning for google-cloud-core
    
    * change to protobuf
    
    * changing tox version
    
    * changing versioning for google-cloud-core
    
    * versioning
    
    * dataproc changers
    
    * uncapping dataproc in both settings and dev-requirements
    
    * pinning to minor version
    
    Co-authored-by: colin-rogers-dbt <111200756+colin-rogers-dbt@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-import os
 import sys
-import re
 
 # require python 3.7 or newer
 if sys.version_info < (3, 7):
@@ -9,56 +7,65 @@ if sys.version_info < (3, 7):
     print("Please upgrade to Python 3.7 or higher.")
     sys.exit(1)
 
-
-# require version of setuptools that supports find_namespace_packages
-from setuptools import setup
-
 try:
     from setuptools import find_namespace_packages
 except ImportError:
     # the user has a downlevel version of setuptools.
     print("Error: dbt requires setuptools v40.1.0 or higher.")
-    print('Please upgrade setuptools with "pip install --upgrade setuptools" ' "and try again")
+    print('Please upgrade setuptools with "pip install --upgrade setuptools" and try again')
     sys.exit(1)
 
-
-# pull long description from README
-this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, "README.md")) as f:
-    long_description = f.read()
+from pathlib import Path
+from setuptools import setup
 
 
-# get this package's version from dbt/adapters/<name>/__version__.py
-def _get_plugin_version_dict():
-    _version_path = os.path.join(this_directory, "dbt", "adapters", "bigquery", "__version__.py")
-    _semver = r"""(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)"""
-    _pre = r"""((?P<prekind>a|b|rc)(?P<pre>\d+))?"""
-    _version_pattern = rf"""version\s*=\s*["']{_semver}{_pre}["']"""
-    with open(_version_path) as f:
-        match = re.search(_version_pattern, f.read().strip())
-        if match is None:
-            raise ValueError(f"invalid version at {_version_path}")
-        return match.groupdict()
+# pull the long description from the README
+README = Path(__file__).parent / "README.md"
+
+# used for this adapter's version and in determining the compatible dbt-core version
+VERSION = Path(__file__).parent / "dbt/adapters/bigquery/__version__.py"
 
 
-# require a compatible minor version (~=), prerelease if this is a prerelease
-def _get_dbt_core_version():
-    parts = _get_plugin_version_dict()
-    minor = "{major}.{minor}.0".format(**parts)
-    pre = parts["prekind"] + "1" if parts["prekind"] else ""
-    return f"{minor}{pre}"
+def _dbt_bigquery_version() -> str:
+    """
+    Pull the package version from the main package version file
+    """
+    attributes = {}
+    exec(VERSION.read_text(), attributes)
+    return attributes["version"]
+
+
+# require a compatible minor version (~=) and prerelease if this is a prerelease
+def _dbt_core_version(plugin_version: str) -> str:
+    """
+    Determine the compatible version of dbt-core using this package's version
+    """
+    try:
+        major, minor, plugin_patch = plugin_version.split(".")
+    except ValueError:
+        raise ValueError(f"Invalid version: {plugin_version}")
+
+    pre_release_phase = "".join([i for i in plugin_patch if not i.isdigit()])
+    if pre_release_phase:
+        if pre_release_phase not in ["a", "b", "rc"]:
+            raise ValueError(f"Invalid version: {plugin_version}")
+        core_patch = f"0{pre_release_phase}1"
+    else:
+        core_patch = "0"
+
+    return f"{major}.{minor}.{core_patch}"
 
 
 package_name = "dbt-bigquery"
 package_version = "1.4.0b1"
-dbt_core_version = _get_dbt_core_version()
+dbt_core_version = _dbt_core_version(_dbt_bigquery_version())
 description = """The BigQuery adapter plugin for dbt"""
 
 setup(
-    name=package_name,
-    version=package_version,
-    description=description,
-    long_description=long_description,
+    name="dbt-bigquery",
+    version=_dbt_bigquery_version(),
+    description="The Bigquery adapter plugin for dbt",
+    long_description=README.read_text(),
     long_description_content_type="text/markdown",
     author="dbt Labs",
     author_email="info@dbtlabs.com",
@@ -66,14 +73,14 @@ setup(
     packages=find_namespace_packages(include=["dbt", "dbt.*"]),
     include_package_data=True,
     install_requires=[
-        "dbt-core~={}".format(dbt_core_version),
+        f"dbt-core~={_dbt_core_version(_dbt_bigquery_version())}",
         "protobuf>=3.13.0,<4",
-        "google-cloud-core>=1.3.0,<3",
-        "google-cloud-bigquery>=1.25.0,<3.3.3",
-        "google-api-core>=1.16.0,<3",
-        "googleapis-common-protos>=1.6.0,<2",
-        "google-cloud-storage>=2.4.0",
-        "google-cloud-dataproc>=4.0.3",
+        "google-cloud-core~=2.0",
+        "google-cloud-bigquery~=3.0",
+        "google-api-core~=2.0",
+        "googleapis-common-protos~=1.6",
+        "google-cloud-storage~=2.4",
+        "google-cloud-dataproc~=5.0",
     ],
     zip_safe=False,
     classifiers=[
@@ -86,6 +93,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     python_requires=">=3.7",
 )

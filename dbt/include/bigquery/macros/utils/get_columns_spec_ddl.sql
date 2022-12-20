5eb87b6805968c696ae1134a2fc1abc61a9deb78
Victoria Perez Mola <me.victoria@gmail.com>
2022-12-20 19:01:29 +0100

    update checks validation and constraints list

--- dbt/include/bigquery/macros/utils/get_columns_spec_ddl.sql
+++ dbt/include/bigquery/macros/utils/get_columns_spec_ddl.sql
@@ -1,18 +1,18 @@
 {% macro bigquery__get_columns_spec_ddl() %}
   {# loop through user_provided_columns to create DDL with data types and constraints #}
   {% if config.get('constraints_enabled', False) %}
+    {%- set ns = namespace(at_least_one_check=False) -%}
     {%- set user_provided_columns = model['columns'] -%}
     (
     {% for i in user_provided_columns -%}
       {%- set col = user_provided_columns[i] -%}
       {% set constraints = col['constraint'] -%}
-      {%- set checks = col['check'] -%}
-      {%- if checks -%}
-        {{exceptions.warn("We noticed you have `check` in your configs, these are NOT compatible with BigQuery and will be ignored")}}
-      {%- endif %}
-      {{ col['name'] }} {{ col['data_type'] }} {{ col['constraint']  or "" }} {{ "," if not loop.last }}
+      {%- set ns.at_least_one_check = ns.at_least_one_check or col['check'] %}
+      {{ col['name'] }} {{ col['data_type'] }} {% for x in constraints %} {{ x or "" }} {% endfor %} {{ "," if not loop.last }}
     {%- endfor %}
   )
+  {%- if ns.at_least_one_check -%}
+      {{exceptions.warn("We noticed you have `check` configs, these are NOT compatible with BigQuery and will be ignored")}}
+  {%- endif %}
   {% endif %}
 {% endmacro %}
-

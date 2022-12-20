60dda354c185726085d78da840ca59065271d055
Victoria Perez Mola <me.victoria@gmail.com>
2022-12-20 22:28:15 +0100

    Update test_bigquery_constraints.py

--- tests/functional/adapter/constraints/test_bigquery_constraints.py
+++ tests/functional/adapter/constraints/test_bigquery_constraints.py
@@ -78,7 +78,7 @@ schema_name = "schema_placeholder"
 
 _expected_sql = f"""
   create or replace table
-    "{database_name}"."{schema_name}"."my_model__dbt_tmp"
+    "{database_name}"."{schema_name}"."my_model"
     
     (
         id integer not null,

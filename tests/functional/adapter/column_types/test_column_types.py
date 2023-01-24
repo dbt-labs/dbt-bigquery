481ff5ff6971d1bba1c6e1c66c14b95b26a572f5
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2023-01-24 14:05:22 -0600

    revert back to having test specific fixtuers part of test file as they are not overly large additions

--- tests/functional/adapter/column_types/test_column_types.py
+++ tests/functional/adapter/column_types/test_column_types.py
@@ -1,9 +1,26 @@
 import pytest
 from dbt.tests.adapter.column_types.test_column_types import BaseColumnTypes
-from tests.functional.adapter.column_types.fixtures import (
-    _MODEL_SQL,
-    _SCHEMA_YML
-)
+
+_MODEL_SQL = """
+select
+    CAST(1 as int64) as int64_col,
+    CAST(2.0 as float64) as float64_col,
+    CAST(3.0 as numeric) as numeric_col,
+    CAST('3' as string) as string_col,
+"""
+
+_SCHEMA_YML = """
+version: 2
+models:
+  - name: model
+    tests:
+      - is_type:
+          column_map:
+            int64_col: ['integer', 'number']
+            float64_col: ['float', 'number']
+            numeric_col: ['numeric', 'number']
+            string_col: ['string', 'not number']
+"""
 
 class TestBigQueryColumnTypes(BaseColumnTypes):
 

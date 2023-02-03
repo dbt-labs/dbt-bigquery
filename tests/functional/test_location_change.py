36f5f105cde7674c6410c9448fca3cb170439ea8
Matthew McKnight <91097623+McKnight-42@users.noreply.github.com>
2023-02-02 23:53:29 -0600

    BigQuery_tests conversion (#492)
    * init push for tests conversions in bigquery_tests
    
    * add changing_partitions test conversion and update to main
    
    * move tests outside of sub folder
    
    * adding a revert on the location param to not break next test
    
    * spacing fix
    
    * modify fixture for test to have way to revert to default location
    
    * add a try, finally condition as possible extra protection
    
    * remove unneeded try finally condtion
    
    * remove unused arg

---
+++ tests/functional/test_location_change.py
@@ -0,0 +1,42 @@
+import pytest
+import os
+from dbt.tests.util import run_dbt
+
+_MODEL_SQL = """
+select 1 as id
+"""
+
+_INVALID_LOCATION = os.getenv('DBT_TEST_BIGQUERY_BAD_LOCATION', 'northamerica-northeast1')
+_VALID_LOCATION = os.getenv('DBT_TEST_BIGQUERY_INITIAL_LOCATION', 'US')
+
+class BaseBigQueryLocation:
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "model.sql": _MODEL_SQL,
+        }
+
+
+class TestBigqueryValidLocation(BaseBigQueryLocation):
+
+    def test_bigquery_valid_location(self, project):
+        results = run_dbt()
+        for result in results:
+            assert "US" == result.adapter_response["location"]
+
+
+class TestBigqueryInvalidLocation(BaseBigQueryLocation):
+
+    @pytest.fixture(scope="class")
+    def profiles_config_update(self, dbt_profile_target):
+        outputs = {"default": dbt_profile_target}
+        outputs["default"]["location"] = _INVALID_LOCATION
+        yield
+        outputs = {"default": dbt_profile_target}
+        outputs["default"]["location"] = _VALID_LOCATION
+
+    def test_bigquery_location_invalid(self, project):
+        results = run_dbt()
+        for result in results:
+            assert "northamerica-northeast1" == result.adapter_response["location"]

72aa10a610524896b8bf4baaa13d1762e27fa47e
Michelle Ark <michelle.ark@dbtlabs.com>
2023-03-02 19:40:02 -0500

    test more types! + correct data_type schemas

--- tests/functional/adapter/test_constraints.py
+++ tests/functional/adapter/test_constraints.py
@@ -9,7 +9,6 @@ from dbt.tests.adapter.constraints.fixtures import (
     my_model_wrong_order_sql,
     my_model_wrong_name_sql,
     model_schema_yml,
-    my_model_wrong_data_type_sql,
 )
 
 _expected_sql_bigquery = """
@@ -30,7 +29,7 @@ as (
 # Different on BigQuery:
 # - does not support a data type named 'text' (TODO handle this via type translation/aliasing!)
 # - raises an explicit error, if you try to set a primary key constraint, because it's not enforced
-constraints_yml = model_schema_yml.replace("text", "string").replace("int[]", "ARRAY<INT>").replace("primary key", "")
+constraints_yml = model_schema_yml.replace("text", "string").replace("primary key", "")
 
 
 class TestBigQueryConstraintsColumnsEqual(BaseConstraintsColumnsEqual):
@@ -39,7 +38,6 @@ class TestBigQueryConstraintsColumnsEqual(BaseConstraintsColumnsEqual):
         return {
             "my_model_wrong_order.sql": my_model_wrong_order_sql,
             "my_model_wrong_name.sql": my_model_wrong_name_sql,
-            "my_model_wrong_data_type.sql": my_model_wrong_data_type_sql,
             "constraints_schema.yml": constraints_yml,
         }
 
@@ -52,12 +50,20 @@ class TestBigQueryConstraintsColumnsEqual(BaseConstraintsColumnsEqual):
         return "INT64"
 
     @pytest.fixture
-    def int_array_type(self):
-        return "ARRAY<INT64>"
-
-    @pytest.fixture
-    def string_array_type(self):
-        return "ARRAY<STRING>"
+    def data_types(self, int_type, string_type):
+        # sql_column_value, schema_data_type, error_data_type
+        return [
+            ['1', int_type, int_type],
+            ["'1'", string_type, string_type],
+            ["cast('2019-01-01' as date)", 'date', 'DATE'],
+            ["true", 'bool', 'BOOL'],
+            ["cast('2013-11-03 00:00:00-07' as TIMESTAMP)", 'timestamp', 'TIMESTAMP'],
+            ["['a','b','c']", f'ARRAY<{string_type}>', f'ARRAY<{string_type}>'],
+            ["[1,2,3]", f'ARRAY<{int_type}>', f'ARRAY<{int_type}>'],
+            ["cast(1 as NUMERIC)", 'numeric', 'NUMERIC'],
+            ["""JSON '{"name": "Cooper", "forname": "Alice"}'""", 'json', 'JSON'],
+            ['STRUCT("Rudisha" AS name, [23.4, 26.3, 26.4, 26.1] AS laps)', 'STRUCT<name STRING, laps ARRAY<FLOAT64>>', 'STRUCT<name STRING, laps ARRAY<FLOAT64>>']
+        ]
 
 
 class TestBigQueryConstraintsRuntimeEnforcement(BaseConstraintsRuntimeEnforcement):

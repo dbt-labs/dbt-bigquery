9543303636e024e324867c8bf6492f4f5b69a9c3
Neelesh Srinivas Salian <neeleshssalian@gmail.com>
2023-04-11 18:47:28 -0700

    Nit and remove old file

--- tests/functional/adapter/test_simple_snapshot.py
+++
@@ -1,9 +0,0 @@
-from dbt.tests.adapter.simple_snapshot.test_snapshot import BaseSnapshotCheck, BaseSimpleSnapshot
-
-
-class TestSnapshot(BaseSimpleSnapshot):
-    pass
-
-
-class TestSnapshotCheck(BaseSnapshotCheck):
-    pass

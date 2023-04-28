23fbc6d9a0ac8f5238c59dce8fc428fa1e2ef570
Daniel Green <dev@danielgreen.uk>
2023-04-28 23:30:45 +0100

    Fixes adding policy tags when a struct is defined in the yml

--- dbt/adapters/bigquery/impl.py
+++ dbt/adapters/bigquery/impl.py
@@ -693,7 +693,8 @@ class BigQueryAdapter(BaseAdapter):
         if dotted_column_name in dbt_columns:
             column_config = dbt_columns[dotted_column_name]
             bq_column_dict["description"] = column_config.get("description")
-            bq_column_dict["policyTags"] = {"names": column_config.get("policy_tags", list())}
+            if bq_column_dict["type"] != "RECORD":
+                bq_column_dict["policyTags"] = {"names": column_config.get("policy_tags", list())}
 
         new_fields = []
         for child_col_dict in bq_column_dict.get("fields", list()):

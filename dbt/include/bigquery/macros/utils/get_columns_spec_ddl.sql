d055383e7522ba157152f858fe390d0fc0b134ae
Michelle Ark <michelle.ark@dbtlabs.com>
2023-05-26 12:59:17 -0400

    cleanup

--- dbt/include/bigquery/macros/utils/get_columns_spec_ddl.sql
+++ dbt/include/bigquery/macros/utils/get_columns_spec_ddl.sql
@@ -19,21 +19,3 @@
         {{ sql }}
     ) as model_subq
 {%- endmacro %}
-
-{% macro bigquery__get_table_columns_and_constraints() -%}
-  {{ return(bigquery_table_columns_and_constraints()) }}
-{%- endmacro %}
-
-{% macro bigquery_table_columns_and_constraints() %}
-  {# loop through user_provided_columns to create DDL with data types and constraints #}
-    {%- set raw_column_constraints = adapter.render_raw_columns_constraints(raw_columns=model['columns']) -%}
-    {%- set raw_model_constraints = adapter.render_raw_model_constraints(raw_constraints=model['constraints']) -%}
-    (
-    {% for c in raw_column_constraints -%}
-      {{ c }}{{ "," if not loop.last or raw_model_constraints }}
-    {% endfor %}
-    {% for c in raw_model_constraints -%}
-        {{ c }}{{ "," if not loop.last }}
-    {% endfor -%}
-    )
-{% endmacro %}

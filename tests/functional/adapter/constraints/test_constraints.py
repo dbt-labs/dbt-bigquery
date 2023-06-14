8da4f4b74f63d3a288e681697f5e8cc958f17b5e
Michelle Ark <MichelleArk@users.noreply.github.com>
2023-06-14 10:34:49 -0700

    model contracts + constraints with nested fields (#738)
    (cherry picked from commit b1e950bf6f8f6c3528b65080e257b746839ef87c)

--- tests/functional/adapter/constraints/test_constraints.py
+++ tests/functional/adapter/constraints/test_constraints.py
@@ -26,6 +26,19 @@ from dbt.tests.adapter.constraints.fixtures import (
     my_model_incremental_wrong_order_depends_on_fk_sql,
 )
 
+from tests.functional.adapter.constraints.fixtures import (
+    my_model_struct_wrong_data_type_sql,
+    my_model_struct_correct_data_type_sql,
+    my_model_double_struct_wrong_data_type_sql,
+    my_model_double_struct_correct_data_type_sql,
+    model_struct_data_type_schema_yml,
+    model_double_struct_data_type_schema_yml,
+    my_model_struct_sql,
+    model_struct_schema_yml,
+)
+
+from dbt.tests.util import run_dbt_and_capture, run_dbt
+
 _expected_sql_bigquery = """
 create or replace table <model_identifier> (
     id integer not null primary key not enforced references <foreign_key_model_identifier> (id) not enforced,
@@ -42,6 +55,19 @@ as (
     select 'blue' as color,
     1 as id,
     '2019-01-01' as date_day
+  ) as model_subq
+);
+"""
+
+_expected_struct_sql_bigquery = """
+create or replace table <model_identifier> (
+    id struct<nested_column string not null, nested_column2 string>
+)
+OPTIONS()
+as (
+    select id from
+  (
+    select STRUCT("test" as nested_column, "test" as nested_column2) as id
   ) as model_subq
 );
 """
@@ -68,14 +94,13 @@ class BigQueryColumnEqualSetup:
         return [
             ['1', int_type, int_type],
             ["'1'", string_type, string_type],
-            ["cast('2019-01-01' as date)", 'date', 'DATE'],
-            ["true", 'bool', 'BOOL'],
-            ["cast('2013-11-03 00:00:00-07' as TIMESTAMP)", 'timestamp', 'TIMESTAMP'],
-            ["['a','b','c']", f'ARRAY<{string_type}>', f'ARRAY<{string_type}>'],
-            ["[1,2,3]", f'ARRAY<{int_type}>', f'ARRAY<{int_type}>'],
-            ["cast(1 as NUMERIC)", 'numeric', 'NUMERIC'],
-            ["""JSON '{"name": "Cooper", "forname": "Alice"}'""", 'json', 'JSON'],
-            ['STRUCT("Rudisha" AS name, [23.4, 26.3, 26.4, 26.1] AS laps)', 'STRUCT<name STRING, laps ARRAY<FLOAT64>>', 'STRUCT<name STRING, laps ARRAY<FLOAT64>>']
+            ["cast('2019-01-01' as date)", "date", "DATE"],
+            ["true", "bool", "BOOL"],
+            ["cast('2013-11-03 00:00:00-07' as TIMESTAMP)", "timestamp", "TIMESTAMP"],
+            ["['a','b','c']", f"ARRAY<{string_type}>", f"ARRAY<{string_type}>"],
+            ["[1,2,3]", f"ARRAY<{int_type}>", f"ARRAY<{int_type}>"],
+            ["cast(1 as NUMERIC)", "numeric", "NUMERIC"],
+            ["""JSON '{"name": "Cooper", "forname": "Alice"}'""", "json", "JSON"],
         ]
 
 
@@ -118,9 +143,62 @@ class TestBigQueryIncrementalConstraintsColumnsEqual(
         }
 
 
-class TestBigQueryTableConstraintsRuntimeDdlEnforcement(
-    BaseConstraintsRuntimeDdlEnforcement
-):
+class BaseStructContract:
+    @pytest.fixture
+    def wrong_schema_data_type(self):
+        return "INT64"
+
+    @pytest.fixture
+    def correct_schema_data_type(self):
+        return "STRING"
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "contract_struct_schema.yml": model_struct_data_type_schema_yml,
+            "contract_struct_wrong.sql": my_model_struct_wrong_data_type_sql,
+            "contract_struct_correct.sql": my_model_struct_correct_data_type_sql,
+        }
+
+    def test__struct_contract_wrong_data_type(
+        self, project, correct_schema_data_type, wrong_schema_data_type
+    ):
+        results, log_output = run_dbt_and_capture(
+            ["run", "-s", "contract_struct_wrong"], expect_pass=False
+        )
+        assert len(results) == 1
+        assert results[0].node.config.contract.enforced
+
+        expected = [
+            "struct_column_being_tested",
+            wrong_schema_data_type,
+            correct_schema_data_type,
+            "data type mismatch",
+        ]
+        assert all([(exp in log_output or exp.upper() in log_output) for exp in expected])
+
+    def test__struct_contract_correct_data_type(self, project):
+        results = run_dbt(["run", "-s", "contract_struct_correct"])
+
+        assert len(results) == 1
+        assert results[0].node.config.contract.enforced
+
+
+class TestBigQueryStructContract(BaseStructContract):
+    pass
+
+
+class TestBigQueryDoubleStructContract(BaseStructContract):
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "contract_struct_schema.yml": model_double_struct_data_type_schema_yml,
+            "contract_struct_wrong.sql": my_model_double_struct_wrong_data_type_sql,
+            "contract_struct_correct.sql": my_model_double_struct_correct_data_type_sql,
+        }
+
+
+class TestBigQueryTableConstraintsRuntimeDdlEnforcement(BaseConstraintsRuntimeDdlEnforcement):
     @pytest.fixture(scope="class")
     def models(self):
         return {
@@ -134,9 +212,22 @@ class TestBigQueryTableConstraintsRuntimeDdlEnforcement(
         return _expected_sql_bigquery
 
 
-class TestBigQueryTableConstraintsRollback(
-    BaseConstraintsRollback
+class TestBigQueryStructTableConstraintsRuntimeDdlEnforcement(
+    BaseConstraintsRuntimeDdlEnforcement
 ):
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "my_model.sql": my_model_struct_sql,
+            "constraints_schema.yml": model_struct_schema_yml,
+        }
+
+    @pytest.fixture(scope="class")
+    def expected_sql(self, project):
+        return _expected_struct_sql_bigquery
+
+
+class TestBigQueryTableConstraintsRollback(BaseConstraintsRollback):
     @pytest.fixture(scope="class")
     def models(self):
         return {

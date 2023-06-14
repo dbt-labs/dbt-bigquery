b1e950bf6f8f6c3528b65080e257b746839ef87c
Michelle Ark <MichelleArk@users.noreply.github.com>
2023-06-14 10:34:49 -0700

    model contracts + constraints with nested fields (#738)

--- dbt/include/bigquery/macros/utils/get_columns_spec_ddl.sql
+++ dbt/include/bigquery/macros/utils/get_columns_spec_ddl.sql
@@ -3,3 +3,25 @@
   {% set formatted = column.column.lower() ~ " " ~ data_type %}
   {{ return({'name': column.name, 'data_type': data_type, 'formatted': formatted}) }}
 {%- endmacro -%}
+
+{% macro bigquery__get_empty_schema_sql(columns) %}
+    {%- set columns = adapter.nest_column_data_types(columns) -%}
+    {{ return(dbt.default__get_empty_schema_sql(columns)) }}
+{% endmacro %}
+
+{% macro bigquery__get_select_subquery(sql) %}
+    select {{ adapter.dispatch('get_column_names')() }}
+    from (
+        {{ sql }}
+    ) as model_subq
+{%- endmacro %}
+
+{% macro bigquery__get_column_names() %}
+  {#- loop through nested user_provided_columns to get column names -#}
+    {%- set user_provided_columns = adapter.nest_column_data_types(model['columns']) -%}
+    {%- for i in user_provided_columns %}
+      {%- set col = user_provided_columns[i] -%}
+      {%- set col_name = adapter.quote(col['name']) if col.get('quote') else col['name'] -%}
+      {{ col_name }}{{ ", " if not loop.last }}
+    {%- endfor -%}
+{% endmacro %}

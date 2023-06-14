d97442f047f1f363bf6aa0d1c99ca5d87a3da285
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2023-06-14 12:12:16 -0500

    remove db api files as shift was considered out of scope

--- dbt/include/bigquery/macros/db_api/materialized_view/create.sql
+++
@@ -1,4 +0,0 @@
-{% macro bigquery__db_api__materialized_view__create(relation, sql) %}
-    {% set proxy_view = bigquery__create_view_as(relation, sql) %}
-    {{ return(proxy_view) }}
-{% endmacro %}

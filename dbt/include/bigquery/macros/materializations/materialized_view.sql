b99e4e5b52b3a2dbe12a67e5969abb02d16e5b2a
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2023-06-15 12:44:57 -0500

    add very rough draft of bigquery version of materialization

--- dbt/include/bigquery/macros/materializations/materialized_view.sql
+++ dbt/include/bigquery/macros/materializations/materialized_view.sql
@@ -1,5 +1,3 @@
-
-
 {% macro bigquery__get_create_materialized_view_as_sql(relation, sql) %}
     create materialized view if not exists {{ relation }} as {{ sql }}
 {% endmacro %}
@@ -49,3 +47,25 @@
 {% macro bigquery__get_materialized_view_configuration_changes() %}
     return none
 {% endmacro %}
+
+{% materialization materialized_view, adapter='bigquery' %}
+    {% set existing_relation = load_cached_relation(this) %}
+    {% set target_relation = this.incorporate(type=this.MaterializedView) %}
+    {% set intermediate_relation = make_intermediate_relation(target_relation) %}
+
+
+    {{ materialized_view_setup(backup_relation, intermediate_relation, pre_hooks) }}
+
+        {% set build_sql = materialized_view_get_build_sql(existing_relation, target_relation, backup_relation, intermediate_relation) %}
+
+        {% if build_sql == '' %}
+            {{ materialized_view_execute_no_op(target_relation) }}
+        {% else %}
+            {{ materialized_view_execute_build_sql(build_sql, existing_relation, target_relation, post_hooks) }}
+        {% endif %}
+
+    {{ materialized_view_teardown(backup_relation, intermediate_relation, post_hooks) }}
+
+    {{ return({'relations': [target_relation]}) }}
+
+{% endmaterialization %}

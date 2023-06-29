b08323d691c89f0306a2bd4f95b1c46f0fb05774
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2023-06-29 16:31:16 -0500

    add clean_up method for store_test_failures tests to stop hanging artifacts in bigquery

--- tests/functional/adapter/store_test_failures_tests/test_store_test_failures.py
+++ tests/functional/adapter/store_test_failures_tests/test_store_test_failures.py
@@ -1,9 +1,22 @@
+import pytest
 from dbt.tests.adapter.store_test_failures_tests.test_store_test_failures import (
     StoreTestFailuresBase,
 )
 
 
+TEST_AUDIT_SCHEMA_SUFFIX = "dbt_test__aud"
+
+
 class TestBigQueryStoreTestFailures(StoreTestFailuresBase):
-    def test_store_and_assert(self, project):
+    @pytest.fixture(scope="function")
+    def clean_up(self, project):
+        yield
+        relation = project.adapter.Relation.create(
+            database=project.database, schema=f"{project.test_schema}_{TEST_AUDIT_SCHEMA_SUFFIX}"
+        )
+
+        project.adapter.drop_schema(relation)
+
+    def test_store_and_assert(self, project, clean_up):
         self.run_tests_store_one_failure(project)
         self.run_tests_store_failures_and_assert(project)

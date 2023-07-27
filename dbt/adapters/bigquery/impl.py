86652bd16385248b1eac255472dfd1bd2a0939e3
Christophe Oudar <kayrnt@gmail.com>
2023-07-27 22:38:00 +0200

    Use execute instead of raw execute to add the query comment as query header

--- dbt/adapters/bigquery/impl.py
+++ dbt/adapters/bigquery/impl.py
@@ -399,7 +399,7 @@ class BigQueryAdapter(BaseAdapter):
         :param str sql: The sql to execute.
         :return: List[BigQueryColumn]
         """
-        _, iterator = self.connections.raw_execute(sql)
+        _, iterator = self.connections.execute(sql)
         columns = [self.Column.create_from_field(field) for field in iterator.schema]
         flattened_columns = []
         for column in columns:
@@ -411,7 +411,7 @@ class BigQueryAdapter(BaseAdapter):
         try:
             conn = self.connections.get_thread_connection()
             client = conn.handle
-            query_job, iterator = self.connections.raw_execute(select_sql)
+            query_job, iterator = self.connections.execute(select_sql)
             query_table = client.get_table(query_job.destination)
             return self._get_dbt_columns_from_bq_table(query_table)
 

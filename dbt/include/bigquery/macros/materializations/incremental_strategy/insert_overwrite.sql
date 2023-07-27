f54981e9aa08336881160d46a5f5ce8e9bc5e5e6
Christophe Oudar <kayrnt@gmail.com>
2023-07-27 21:16:03 +0200

    avoid creating twice the temp table in dynamic insert overwrite

--- dbt/include/bigquery/macros/materializations/incremental_strategy/insert_overwrite.sql
+++ dbt/include/bigquery/macros/materializations/incremental_strategy/insert_overwrite.sql
@@ -97,11 +97,13 @@
 {% macro bq_dynamic_copy_partitions_insert_overwrite_sql(
   tmp_relation, target_relation, sql, unique_key, partition_by, dest_columns, tmp_relation_exists, copy_partitions
   ) %}
-  {# We run temp table creation in a separated script to move to partitions copy #}
-  {%- call statement('create_tmp_relation_for_copy', language='sql') -%}
-    {{ bq_create_table_as(partition_by, True, tmp_relation, sql, 'sql')
-  }}
-  {%- endcall %}
+  {%- if tmp_relation_exists is false -%}
+  {# We run temp table creation in a separated script to move to partitions copy if it doesn't already exist #}
+    {%- call statement('create_tmp_relation_for_copy', language='sql') -%}
+      {{ bq_create_table_as(partition_by, True, tmp_relation, sql, 'sql')
+    }}
+    {%- endcall %}
+  {%- endif -%}
   {%- set partitions_sql -%}
     select distinct {{ partition_by.render_wrapped() }}
     from {{ tmp_relation }}

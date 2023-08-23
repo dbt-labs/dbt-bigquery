1e3a511c1874f073f2c11383319eeb96e998185d
Matthew McKnight <91097623+McKnight-42@users.noreply.github.com>
2023-08-23 11:08:35 -0500

    case insensitive check on partition matching (#888) (#892)
    * case insensitive check on partition matching
    
    * Review change
    
    ---------
    
    Co-authored-by: Christophe Oudar <90898980+github-christophe-oudar@users.noreply.github.com>
    Co-authored-by: Christophe Oudar <kayrnt@gmail.com>

--- dbt/adapters/bigquery/__init__.py
+++ dbt/adapters/bigquery/__init__.py
@@ -2,7 +2,7 @@ from dbt.adapters.bigquery.connections import BigQueryConnectionManager  # noqa
 from dbt.adapters.bigquery.connections import BigQueryCredentials
 from dbt.adapters.bigquery.relation import BigQueryRelation  # noqa
 from dbt.adapters.bigquery.column import BigQueryColumn  # noqa
-from dbt.adapters.bigquery.impl import BigQueryAdapter, GrantTarget  # noqa
+from dbt.adapters.bigquery.impl import BigQueryAdapter, GrantTarget, PartitionConfig  # noqa
 
 from dbt.adapters.base import AdapterPlugin
 from dbt.include import bigquery

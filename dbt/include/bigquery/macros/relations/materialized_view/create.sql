47b787d270785361608e65f1017f895a311ec804
Mike Alfare <mike.alfare@dbtlabs.com>
2023-09-27 19:03:43 -0400

    update create statement to include partition, cluster, and options clauses

--- dbt/include/bigquery/macros/relations/materialized_view/create.sql
+++ dbt/include/bigquery/macros/relations/materialized_view/create.sql
@@ -1,3 +1,17 @@
 {% macro bigquery__get_create_materialized_view_as_sql(relation, sql) %}
-    create materialized view if not exists {{ relation }} as {{ sql }}
+
+    {%- set partition_config_raw = config.get('partition_by', none) -%}
+    {%- set partition_config = adapter.parse_partition_by(partition_config_raw) -%}
+    {%- if partition_config.time_ingestion_partitioning -%}
+        {% do exceptions.raise_compiler_error("Time ingestion partitioning is not supported for materialized views") %}
+    {%- endif -%}
+
+    {%- set cluster_config = config.get('cluster_by', none) -%}
+
+    create materialized view if not exists {{ relation }}
+    {{ partition_by(partition_config) }}
+    {{ cluster_by(cluster_config) }}
+    {{ bigquery_options(adapter.materialized_view_options(config, model)) }}
+    as {{ sql }}
+
 {% endmacro %}

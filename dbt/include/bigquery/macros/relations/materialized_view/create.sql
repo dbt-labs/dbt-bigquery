97b025c5f7d89aadd5b49ec98526200bb25495bb
Mike Alfare <mike.alfare@dbtlabs.com>
2023-09-28 18:52:48 -0400

    update create materialized view to use the relation config

--- dbt/include/bigquery/macros/relations/materialized_view/create.sql
+++ dbt/include/bigquery/macros/relations/materialized_view/create.sql
@@ -1,17 +1,11 @@
 {% macro bigquery__get_create_materialized_view_as_sql(relation, sql) %}
 
-    {%- set partition_config_raw = config.get('partition_by', none) -%}
-    {%- set partition_config = adapter.parse_partition_by(partition_config_raw) -%}
-    {%- if partition_config.time_ingestion_partitioning -%}
-        {% do exceptions.raise_compiler_error("Time ingestion partitioning is not supported for materialized views") %}
-    {%- endif -%}
-
-    {%- set cluster_config = config.get('cluster_by', none) -%}
+    {%- set materialized_view = adapter.Relation.materialized_view_from_model_node(model) -%}
 
     create materialized view if not exists {{ relation }}
-    {{ partition_by(partition_config) }}
-    {{ cluster_by(cluster_config) }}
-    {{ bigquery_options(adapter.materialized_view_options(config, model)) }}
+    {{ partition_by(materialized_view.partition) }}
+    {{ cluster_by(materialized_view.cluster.fields) }}
+    {{ bigquery_options(adapter.materialized_view_options(materialized_view)) }}
     as {{ sql }}
 
 {% endmacro %}

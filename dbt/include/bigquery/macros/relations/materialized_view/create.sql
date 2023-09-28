d08c54b128452300e7f54de81582d627404b0d43
Mike Alfare <mike.alfare@dbtlabs.com>
2023-09-28 19:20:52 -0400

    condition on existence of properties before templating them

--- dbt/include/bigquery/macros/relations/materialized_view/create.sql
+++ dbt/include/bigquery/macros/relations/materialized_view/create.sql
@@ -1,11 +1,11 @@
 {% macro bigquery__get_create_materialized_view_as_sql(relation, sql) %}
 
-    {%- set materialized_view = adapter.Relation.materialized_view_from_model_node(model) -%}
+    {%- set materialized_view = adapter.Relation.materialized_view_from_model_node(config.model) -%}
 
     create materialized view if not exists {{ relation }}
-    {{ partition_by(materialized_view.partition) }}
-    {{ cluster_by(materialized_view.cluster.fields) }}
-    {{ bigquery_options(adapter.materialized_view_options(materialized_view)) }}
+    {% if materialized_view.partition %}{{ partition_by(materialized_view.partition) }}{% endif %}
+    {% if materialized_view.cluster %}{{ cluster_by(materialized_view.cluster.fields) }}{% endif %}
+    {{ bigquery_options(adapter.get_materialized_view_options(materialized_view)) }}
     as {{ sql }}
 
 {% endmacro %}

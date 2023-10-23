a32baf12d9578dc3ad6aa51ff7a32ba472a3308a
Colin <colin.rogers@dbtlabs.com>
2023-10-23 09:32:36 -0700

    add json struct functional test

--- tests/functional/adapter/dbt_show/test_dbt_show.py
+++ tests/functional/adapter/dbt_show/test_dbt_show.py
@@ -1,4 +1,28 @@
+import pytest
 from dbt.tests.adapter.dbt_show.test_dbt_show import BaseShowSqlHeader, BaseShowLimit
+
+from dbt.tests.util import run_dbt
+
+model_with_json_struct = """
+    select *
+    from (
+        select
+  struct<
+    k array<
+        struct<c1 int64, c2 json>
+      >
+  >(
+    [
+      struct(
+        1 as c1,
+        to_json(struct(1 as a)) as c2
+      )
+    ]
+  )
+  as v
+    ) as model_limit_subq
+    limit 5
+    """
 
 
 class TestBigQueryShowLimit(BaseShowLimit):
@@ -7,3 +31,14 @@ class TestBigQueryShowLimit(BaseShowLimit):
 
 class TestBigQueryShowSqlHeader(BaseShowSqlHeader):
     pass
+
+
+class TestBigQueryShowSqlWorksWithJSONStruct:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "json_struct_model.sql": model_with_json_struct,
+        }
+
+    def test_sql_header(self, project):
+        run_dbt(["show", "--select", "json_struct_model"])

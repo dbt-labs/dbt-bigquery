f58fb4d9e0b3c63cddb67c89ccc17d4be1a4e663
Michelle Ark <michelle.ark@dbtlabs.com>
2023-12-04 15:59:34 +0900

    expand test coverage, update safe_cast

--- dbt/include/bigquery/macros/utils/safe_cast.sql
+++ dbt/include/bigquery/macros/utils/safe_cast.sql
@@ -1,3 +1,9 @@
 {% macro bigquery__safe_cast(field, type) %}
+{%- if type.lower().startswith('array') and field is iterable and (field is not string and field is not mapping) and field | length > 0 -%}
+    (select array_agg(safe_cast(i as {{type.lower()[6:-1]}})) from unnest({{field}}) i)
+{%- elif type.lower() == 'json' and field is mapping -%}
+    safe_cast(json {{ dbt.string_literal(tojson(field)) }} as json)
+{%- else -%}
     safe_cast({{field}} as {{type}})
+{%- endif -%}
 {% endmacro %}
--- tests/functional/adapter/unit_testing/test_unit_testing_types.py
+++ tests/functional/adapter/unit_testing/test_unit_testing_types.py
@@ -10,10 +10,27 @@ class TestBigQueryUnitTestingTypes(BaseUnitTestingTypes):
             ["1", "1"],
             ["'1'", "1"],
             ["cast('true' as boolean)", "true"],
+            ["1.0", "1.0"],
+            ["'string value'", "string value"],
+            ["cast(1.0 as numeric)", "1.0"],
+            ["cast(1 as bigint)", 1],
             ["cast('2019-01-01' as date)", "2019-01-01"],
-            ["cast('2013-11-03 00:00:00-07' as TIMESTAMP)", "2013-11-03 00:00:00-07"],
+            ["cast('2013-11-03 00:00:00-07' as timestamp)", "2013-11-03 00:00:00-07"],
             ["cast(['a','b','c'] as array<string>)", "['a','b','c']"],
             ["cast([1,2,3] as array<int>)", "[1,2,3]"],
-            ["cast(1 as NUMERIC)", "1"],
-            # ["""JSON '{"name": "Cooper", "forname": "Alice"}'""", """'{"name": "Cooper", "forname": "Alice"}'"""], #TODO
+            ["cast([true,true,false] as array<bool>)", "[true,true,false]"],
+            # array of date
+            ["[date '2019-01-01']", "['2020-01-01']"],
+            ["[date '2019-01-01']", "[]"],
+            ["[date '2019-01-01']", "null"],
+            # array of timestamp
+            ["[timestamp '2019-01-01']", "['2020-01-01']"],
+            ["[timestamp '2019-01-01']", "[]"],
+            ["[timestamp '2019-01-01']", "null"],
+            # json
+            [
+                """json '{"name": "Cooper", "forname": "Alice"}'""",
+                """{"name": "Cooper", "forname": "Alice"}""",
+            ],
+            ["""json '{"name": "Cooper", "forname": "Alice"}'""", "{}"],
         ]

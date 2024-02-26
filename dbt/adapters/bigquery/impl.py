7f7ca3af48f0873bda8a3aedd0aca8c25704a9da
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2024-02-26 16:31:07 +0900

    Show debug log in `get_relation` if table isn't found
    Signed-off-by: Yu Ishikawa <yu-iskw@users.noreply.github.com>

--- dbt/adapters/bigquery/impl.py
+++ dbt/adapters/bigquery/impl.py
@@ -296,7 +296,8 @@ class BigQueryAdapter(BaseAdapter):
 
         try:
             table = self.connections.get_bq_table(database, schema, identifier)
-        except google.api_core.exceptions.NotFound:
+        except google.api_core.exceptions.NotFound as e:
+            logger.debug("Table not found for database: {}, schema: {}, identifier: {}: {}".format(database, schema, identifier, str(e)))
             table = None
         return self._bq_table_to_relation(table)
 

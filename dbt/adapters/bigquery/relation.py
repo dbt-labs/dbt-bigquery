9a23bc6a29f93f418deeb069968da50cac677529
Mila Page <versusfacit@users.noreply.github.com>
2024-02-27 01:45:07 -0800

    More cherry pick from 1.8 again

--- dbt/adapters/bigquery/relation.py
+++ dbt/adapters/bigquery/relation.py
@@ -113,9 +113,7 @@ class BigQueryRelation(BaseRelation):
             return config_change_collection
         return None
 
-    def information_schema(
-        self, identifier: Optional[str] = None
-    ) -> "BigQueryInformationSchema":
+    def information_schema(self, identifier: Optional[str] = None) -> "BigQueryInformationSchema":
         return BigQueryInformationSchema.from_relation(self, identifier)
 
 
@@ -157,7 +155,9 @@ class BigQueryInformationSchema(InformationSchema):
             # OBJECT_PRIVILEGES require a location.  If the location is blank there is nothing
             # the user can do about it.
             if not relation.location:
-                msg = f'No location/region found when trying to retrieve "{information_schema_view}"'
+                msg = (
+                    f'No location/region found when trying to retrieve "{information_schema_view}"'
+                )
                 raise CompilationError(msg)
             info_schema = info_schema.incorporate(location=relation.location)
         return info_schema

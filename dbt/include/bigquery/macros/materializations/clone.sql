15a45cf1b74539bec0c26810dcf21d01bda0c3e4
Baiyue Cao <caobaiyue@gmail.com>
2024-03-29 10:32:10 -0400

    When cloning, if there is a parition or cluster change, hard drop the table before create clone

--- dbt/include/bigquery/macros/materializations/clone.sql
+++ dbt/include/bigquery/macros/materializations/clone.sql
@@ -3,6 +3,17 @@
 {% endmacro %}
 
 {% macro bigquery__create_or_replace_clone(this_relation, defer_relation) %}
+    {%- set full_refresh_mode = (should_full_refresh()) -%}
+    {%- set raw_partition_by = config.get('partition_by', none) -%}
+    {%- set partition_by = adapter.parse_partition_by(raw_partition_by) -%}
+    {%- set partitions = config.get('partitions', none) -%}
+    {%- set cluster_by = config.get('cluster_by', none) -%}
+    
+    {% if not adapter.is_replaceable(this_relation, partition_by, cluster_by) %}
+        {% do log("Hard refreshing " ~ this_relation ~ " because it is not replaceable") %}
+        {{ adapter.drop_relation(this_relation) }}
+    {% endif %}
+   
     create or replace
       table {{ this_relation }}
       clone {{ defer_relation }}

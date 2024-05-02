c74e75b190b2ced0eea87f42ffb5444ca18ebf06
Mike Alfare <mike.alfare@dbtlabs.com>
2024-05-02 09:47:35 -0400

    cleanup test scripts

--- docker/test.sh
+++ docker/test.sh
@@ -1,22 +1,19 @@
 # - VERY rudimentary test script to run latest + specific branch image builds and test them all by running `--version`
 # TODO: create a real test suite
+set -e
 
-clear \
-&& echo "\n\n"\
-"########################################\n"\
-"##### Testing dbt-bigquery latest #####\n"\
-"########################################\n"\
-&& docker build --tag dbt-bigquery \
-  --target dbt-bigquery \
-  docker \
-&& docker run dbt-bigquery --version \
-\
-&& echo "\n\n"\
-"#########################################\n"\
-"##### Testing dbt-bigquery-1.0.0b1 #####\n"\
-"#########################################\n"\
-&& docker build --tag dbt-bigquery-1.0.0b1 \
-  --target dbt-bigquery \
-  --build-arg commit_ref=v1.0.0b1 \
-  docker \
-&& docker run dbt-bigquery-1.0.0b1 --version
+echo "\n\n"
+echo "#######################################"
+echo "##### Testing dbt-bigquery latest #####"
+echo "#######################################"
+
+docker build --tag dbt-bigquery --target dbt-bigquery docker
+docker run dbt-bigquery --version
+
+echo "\n\n"
+echo "########################################"
+echo "##### Testing dbt-bigquery-1.0.0b1 #####"
+echo "########################################"
+
+docker build --tag dbt-bigquery-1.0.0b1 --target dbt-bigquery --build-arg commit_ref=v1.0.0b1 docker
+docker run dbt-bigquery-1.0.0b1 --version

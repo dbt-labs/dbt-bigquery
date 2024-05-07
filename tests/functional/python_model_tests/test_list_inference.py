a3cb471a5435e18996078a4c206336a93e24e672
Mike Alfare <13974384+mikealfare@users.noreply.github.com>
2024-05-07 17:05:19 -0400

    Add configuration options for `enable_list_inference` and `intermediate_format` for python models (#1205)
    * add configuration options for enable_list_inference and intermediate_format for python models
    * update test classes so that they run as part of the python model tests in tox
    * reconfigure tests to force serial run instead of parallel
    
    (cherry picked from commit 82253130acf188a8b9e180a66f69cc7d47cfcb9b)

---
+++ tests/functional/python_model_tests/test_list_inference.py
@@ -0,0 +1,36 @@
+"""
+This test case addresses this regression: https://github.com/dbt-labs/dbt-bigquery/issues/1047
+
+As the comments point out, the issue appears when the settings are:
+    - list inference: off
+    - intermediate format: parquet
+
+Adjusting either of these alleviates the issue.
+
+When the regression was first reported, `files.MULTI_RECORD` failed while the other models passed.
+"""
+from dbt.tests.util import run_dbt_and_capture
+import pytest
+
+from tests.functional.python_model_tests import files
+
+
+class TestListInference:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            # this is what worked prior to this issue
+            "single_record.py": files.SINGLE_RECORD,
+            # this is the model that initially failed for this issue
+            "multi_record.py": files.MULTI_RECORD,
+            # these are explicit versions of the default settings
+            "enable_list_inference.py": files.ENABLE_LIST_INFERENCE,
+            "enable_list_inference_parquet_format.py": files.ENABLE_LIST_INFERENCE_PARQUET_FORMAT,
+            # orc format also resolves the issue, regardless of list inference
+            "orc_format.py": files.ORC_FORMAT,
+            "disable_list_inference_orc_format.py": files.DISABLE_LIST_INFERENCE_ORC_FORMAT,
+        }
+
+    def test_models_success(self, project, models):
+        result, output = run_dbt_and_capture(["run"])
+        assert len(result) == len(models)

51243e531e30d204c5c5f8cf83b395ed7d1e90ea
Michelle Ark <michelle.ark@dbtlabs.com>
2024-05-13 19:58:17 -0400

    approach 3 tweak: prefilter relation schemas in bigquery__get_relation_last_modified

--- dbt/include/bigquery/macros/metadata.sql
+++ dbt/include/bigquery/macros/metadata.sql
@@ -7,7 +7,14 @@
                {{ current_timestamp() }} as snapshotted_at
         -- TODO: stop hardcoding this
         from `region-us`.INFORMATION_SCHEMA.TABLE_STORAGE
-        where (
+        -- TODO: preprocessing unique schemas would make the query size smaller
+        where (upper(table_schema) IN (
+            {%- for relation in relations -%}
+            upper('{{ relation.schema }}'){%- if not loop.last %},{% endif -%}
+            {%- endfor -%}
+            )
+        )
+        and (
           {%- for relation in relations -%}
             (upper(table_schema) = upper('{{ relation.schema }}') and
              upper(table_name) = upper('{{ relation.identifier }}')){%- if not loop.last %} or {% endif -%}

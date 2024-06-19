b3418de9a3550c805ef2d09550181809a761b832
Michelle Ark <michelle.ark@dbtlabs.com>
2024-06-19 17:57:18 -0400

    generating a fresh job_id for every _query_and_results call

--- dbt/adapters/bigquery/connections.py
+++ dbt/adapters/bigquery/connections.py
@@ -515,11 +515,18 @@ class BigQueryConnectionManager(BaseConnectionManager):
 
         job_creation_timeout = self.get_job_creation_timeout_seconds(conn)
         job_execution_timeout = self.get_job_execution_timeout_seconds(conn)
-        job_id = str(uuid.uuid4())
-        thread_id = self.get_thread_identifier()
-        self.jobs_by_thread[thread_id] = self.jobs_by_thread.get(thread_id, []) + [job_id]
 
         def fn():
+            # Generating a fresh job_id for every _query_and_results call to avoid job_id reuse.
+            # Generating a job id instead of persisting a BigQuery-generated one after client.query is called.
+            # Using BigQuery's job_id can lead to a race condition if a job has been started and a termination
+            # is sent before the job_id was stored, leading to a failure to cancel the job.
+            # By predetermining job_ids (uuid4), we can persist the job_id before the job has been kicked off.
+            # Doing this, the race condition only leads to attempting to cancel a job that doesn't exist.
+            job_id = str(uuid.uuid4())
+            thread_id = self.get_thread_identifier()
+            self.jobs_by_thread[thread_id] = self.jobs_by_thread.get(thread_id, []) + [job_id]
+
             return self._query_and_results(
                 client,
                 sql,

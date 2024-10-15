616de0f525e2927eda648b9a4c75e502d3624129
carolinabtt <carolinabattaglia@hotmail.com>
2024-10-15 19:34:48 +0200

    Fix dynamic materializations with contract enabled

--- dbt/include/bigquery/macros/adapters/columns.sql
+++ dbt/include/bigquery/macros/adapters/columns.sql
@@ -2,6 +2,13 @@
     {%- if select_sql_header is not none -%}
     {{ select_sql_header }}
     {%- endif -%}
+    {%- set contract_config = config.get('contract') -%}
+    {%- if contract_config.enforced and '_dbt_max_partition' in compiled_code -%}
+        declare _dbt_max_partition {{ partition_by.data_type_for_partition() }} default (
+        select max({{ partition_by.field }}) from {{ relation }}
+        where {{ partition_by.field }} is not null
+    );
+    {%- endif -%}
     select * from (
         {{ select_sql }}
     ) as __dbt_sbq

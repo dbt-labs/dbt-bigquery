a2c62b30f0a942abeea5917e2b98f056223b59fb
carolinabtt <carolinabattaglia@hotmail.com>
2024-10-15 19:39:02 +0200

    Partition_by config parameter

--- dbt/include/bigquery/macros/adapters/columns.sql
+++ dbt/include/bigquery/macros/adapters/columns.sql
@@ -4,10 +4,12 @@
     {%- endif -%}
     {%- set contract_config = config.get('contract') -%}
     {%- if contract_config.enforced and '_dbt_max_partition' in compiled_code -%}
+        {%- set raw_partition_by = config.get('partition_by', none) -%}
+        {%- set partition_by = adapter.parse_partition_by(raw_partition_by) -%}
+
         declare _dbt_max_partition {{ partition_by.data_type_for_partition() }} default (
         select max({{ partition_by.field }}) from {{ relation }}
-        where {{ partition_by.field }} is not null
-    );
+        where {{ partition_by.field }} is not null );
     {%- endif -%}
     select * from (
         {{ select_sql }}

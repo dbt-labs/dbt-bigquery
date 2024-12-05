85484a45c681819c4ad246519ad4e7d1ee6de9f6
Michelle Ark <MichelleArk@users.noreply.github.com>
2024-12-05 16:26:52 -0500

    Fix: Cast to timestamp prior to event time comparison (#1422)
    (cherry picked from commit 4d255b2f854d21d5d8871bdaa8d7ab47e7e863a3)

--- tests/functional/adapter/incremental/test_incremental_microbatch.py
+++ tests/functional/adapter/incremental/test_incremental_microbatch.py
@@ -13,6 +13,8 @@ from tests.functional.adapter.incremental.incremental_strategy_fixtures import (
     microbatch_input_sql,
     microbatch_model_no_partition_by_sql,
     microbatch_model_invalid_partition_by_sql,
+    microbatch_input_event_time_date_sql,
+    microbatch_input_event_time_datetime_sql,
 )
 
 
@@ -20,6 +22,32 @@ class TestBigQueryMicrobatch(BaseMicrobatch):
     @pytest.fixture(scope="class")
     def microbatch_model_sql(self) -> str:
         return microbatch_model_no_unique_id_sql
+
+
+class TestBigQueryMicrobatchInputWithDate(TestBigQueryMicrobatch):
+    @pytest.fixture(scope="class")
+    def input_model_sql(self) -> str:
+        return microbatch_input_event_time_date_sql
+
+    @pytest.fixture(scope="class")
+    def insert_two_rows_sql(self, project) -> str:
+        test_schema_relation = project.adapter.Relation.create(
+            database=project.database, schema=project.test_schema
+        )
+        return f"insert into {test_schema_relation}.input_model (id, event_time) values (4, DATE '2020-01-04'), (5, DATE '2020-01-05')"
+
+
+class TestBigQueryMicrobatchInputWithDatetime(TestBigQueryMicrobatch):
+    @pytest.fixture(scope="class")
+    def input_model_sql(self) -> str:
+        return microbatch_input_event_time_datetime_sql
+
+    @pytest.fixture(scope="class")
+    def insert_two_rows_sql(self, project) -> str:
+        test_schema_relation = project.adapter.Relation.create(
+            database=project.database, schema=project.test_schema
+        )
+        return f"insert into {test_schema_relation}.input_model (id, event_time) values (4, DATETIME '2020-01-04'), (5, DATETIME '2020-01-05')"
 
 
 class TestBigQueryMicrobatchMissingPartitionBy:
@@ -30,7 +58,6 @@ class TestBigQueryMicrobatchMissingPartitionBy:
             "input_model.sql": microbatch_input_sql,
         }
 
-    @mock.patch.dict(os.environ, {"DBT_EXPERIMENTAL_MICROBATCH": "True"})
     def test_execution_failure_no_partition_by(self, project):
         with patch_microbatch_end_time("2020-01-03 13:57:00"):
             _, stdout = run_dbt_and_capture(["run"], expect_pass=False)
@@ -45,7 +72,6 @@ class TestBigQueryMicrobatchInvalidPartitionByGranularity:
             "input_model.sql": microbatch_input_sql,
         }
 
-    @mock.patch.dict(os.environ, {"DBT_EXPERIMENTAL_MICROBATCH": "True"})
     def test_execution_failure_no_partition_by(self, project):
         with patch_microbatch_end_time("2020-01-03 13:57:00"):
             _, stdout = run_dbt_and_capture(["run"], expect_pass=False)

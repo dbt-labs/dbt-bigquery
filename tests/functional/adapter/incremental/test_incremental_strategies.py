a6a0b62e62aa5ab2cb8b0fb32f771ce875891187
Borja Vázquez-Barreiros <borjav@spotify.com>
2025-01-21 10:43:12 +0000

    update tests

--- tests/functional/adapter/incremental/test_incremental_strategies.py
+++ tests/functional/adapter/incremental/test_incremental_strategies.py
@@ -90,10 +90,19 @@ class TestBigQueryScripting(SeedConfigBase):
                 "incremental_overwrite_day_with_time_partition_datetime",
                 "incremental_overwrite_day_with_time_partition_expected",
             ),
-            ("incremental_overwrite_static_substrategy_day", "incremental_overwrite_day_expected"),
-            (
+            (   
+                "incremental_overwrite_static_substrategy_day",
+                "incremental_overwrite_day_expected"
+            ),
+            (   
                 "incremental_overwrite_static_substrategy_day_with_deleteinsert",
+                "incremental_overwrite_day_expected",
+            ),
+            (  
                 "incremental_overwrite_static_substrategy_day_with_merge",
+                "incremental_overwrite_day_expected",
+            ),
+            (
                 "incremental_overwrite_static_substrategy_day_with_copy_partitions",
                 "incremental_overwrite_day_expected",
             ),
